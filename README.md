0bc4e2257902475e8f94817d137c110a12c50f7a
Tom Forbes <tom.forbes@theguardian.com>
2019-01-11 13:22:15 +0000

    Basic client with Elm (#2)
    * Serve assets
    
    * Basic elm client
    
    * readme - running locally
    
    * css

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# SAC
+# Support Admin Console
+
+### Running locally
+Build client code and watch for changes:
+```
+npm install
+npm run watch
+```
+
+Run the play server:
+```
+sbt run
+```
--- app/wiring/AppComponents.scala
+++ app/wiring/AppComponents.scala
@@ -2,14 +2,14 @@ package wiring
 
 import com.gu.googleauth.{AntiForgeryChecker, AuthAction, GoogleAuthConfig}
 import play.api.routing.Router
-import controllers.{Application, Login, routes}
+import controllers._
 import play.api.ApplicationLoader.Context
 import play.api.libs.ws.ahc.AhcWSComponents
 import play.api.mvc.AnyContent
 import play.api.{BuiltInComponentsFromContext, NoHttpFiltersComponents}
 import router.Routes
 
-class AppComponents(context: Context) extends BuiltInComponentsFromContext(context) with AhcWSComponents with NoHttpFiltersComponents {
+class AppComponents(context: Context) extends BuiltInComponentsFromContext(context) with AhcWSComponents with NoHttpFiltersComponents with AssetsComponents {
 
   private val authConfig = {
     val clientId = configuration.get[String]("googleAuth.clientId")
@@ -26,6 +26,7 @@ class AppComponents(context: Context) extends BuiltInComponentsFromContext(conte
   override lazy val router: Router = new Routes(
     httpErrorHandler,
     new Application(authAction, controllerComponents),
-    new Login(authConfig, wsClient, controllerComponents)
+    new Login(authConfig, wsClient, controllerComponents),
+    assets
   )
 }
