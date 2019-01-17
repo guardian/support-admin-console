aac9df61eb0c6faf9b13b8f7cec9bf1f55eaebe7
tomrf1 <tom.forbes@theguardian.com>
2019-01-17 16:29:36 +0000

    Revert "Remove serverside data model + form"
    This reverts commit de2f61a63f241e75a7e903612e6a27c848fe7cd8.

---
+++ app/models/SupportFrontendSwitches.scala
@@ -0,0 +1,85 @@
+package models
+
+import io.circe.{Decoder, Encoder}
+import play.api.data._
+import play.api.data.Forms._
+import play.api.data.format.Formatter
+
+/**
+  * Based on https://github.com/guardian/support-frontend/blob/master/app/admin/Settings.scala
+  */
+
+sealed trait SwitchState
+case object On extends SwitchState
+case object Off extends SwitchState
+
+case object SwitchState {
+  implicit val switchStateEncoder: Encoder[SwitchState] = Encoder.encodeString.contramap[SwitchState] {
+    case On => "On"
+    case Off => "Off"
+  }
+
+  implicit val switchStateDecoder: Decoder[SwitchState] = Decoder.decodeString.map {
+    case "On" => On
+    case _ => Off
+  }
+}
+
+case class ExperimentSwitch(name: String, description: String, state: SwitchState)
+
+case class PaymentMethodsSwitch(stripe: SwitchState, payPal: SwitchState, directDebit: Option[SwitchState])
+
+case class SupportFrontendSwitches(
+  oneOffPaymentMethods: PaymentMethodsSwitch,
+  recurringPaymentMethods: PaymentMethodsSwitch,
+  //TODO - make this a Set and add custom serializer for s3? Or change the file
+  experiments: Map[String, ExperimentSwitch],
+  optimize: SwitchState
+)
+
+case class SupportFrontendSettings(switches: SupportFrontendSwitches)
+
+object SupportFrontendSwitches {
+
+  implicit def switchStateFormat: Formatter[SwitchState] = new Formatter[SwitchState] {
+    def bind(key: String, data: Map[String, String]): Either[Seq[FormError], SwitchState] = {
+      data.get(key).collect {
+        case "On" => On
+        case "Off" => Off
+      }.toRight(Seq(FormError(key, "error.required", Nil)))
+    }
+
+    def unbind(key: String, value: SwitchState) = {
+      val s = value match {
+        case On => "On"
+        case Off => "Off"
+      }
+      Map(key -> s)
+    }
+  }
+
+  def mapMapping[A](mapping: Mapping[A]): Mapping[Map[String,A]] = RepeatedMapping(mapping).transform(_.toMap, _.toList)
+
+  val paymentMethodsSwitchMapping = mapping(
+    "stripe" -> of[SwitchState],
+    "payPal" -> of[SwitchState],
+    "directDebit" -> optional(of[SwitchState])
+  )(PaymentMethodsSwitch.apply)(PaymentMethodsSwitch.unapply)
+
+  val experimentSwitchMapping = mapping(
+    "name" -> text,
+    "description" -> text,
+    "state" -> of[SwitchState]
+  )(ExperimentSwitch.apply)(ExperimentSwitch.unapply)
+
+  val supportFrontendSwitchesMapping = mapping(
+    "oneOffPaymentMethods" -> paymentMethodsSwitchMapping,
+    "recurringPaymentMethods" -> paymentMethodsSwitchMapping,
+    "experiments" -> set(experimentSwitchMapping),
+    "optimize" -> of[SwitchState]
+  )(SupportFrontendSwitches.apply)(SupportFrontendSwitches.unapply)
+
+  val supportFrontendSettingsMapping = mapping(
+    "switches" -> supportFrontendSwitchesMapping
+  )(SupportFrontendSettings.apply)(SupportFrontendSettings.unapply)
+}
--- app/services/S3.scala
+++ app/services/S3.scala
@@ -2,23 +2,11 @@ package services
 
 import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
 import com.typesafe.scalalogging.StrictLogging
-import io.circe.{Json, JsonObject}
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.control.NonFatal
 
-import io.circe.parser.parse
-
-case class VersionedS3Json(value: Json, version: String)
-
-object VersionedS3Json {
-  def toJson(versionedS3Data: VersionedS3Json): Json = Json.fromJsonObject {
-    JsonObject(
-      "version" -> Json.fromString(versionedS3Data.version),
-      "value" -> versionedS3Data.value
-    )
-  }
-}
+case class VersionedS3Data(value: String, version: String)
 
 object S3 extends StrictLogging {
   private val s3Client: AmazonS3 = AmazonS3ClientBuilder
@@ -27,22 +15,18 @@ object S3 extends StrictLogging {
     .withCredentials(Aws.credentialsProvider)
     .build()
 
-  def getJson(bucket: String, key: String)(implicit ec: ExecutionContext): Future[Either[String,VersionedS3Json]] = Future {
+  def get(bucket: String, key: String)(implicit ec: ExecutionContext): Future[Either[String,VersionedS3Data]] = Future {
     try {
       val s3Object = s3Client.getObject(bucket, key)
 
       val version = s3Object.getObjectMetadata.getVersionId
+      println(s"version: $version")
 
       val stream = s3Object.getObjectContent
       val result = scala.io.Source.fromInputStream(stream).mkString
       stream.close()
 
-      parse(result) match {
-        case Right(json) => Right[String,VersionedS3Json](VersionedS3Json(json, version))
-        case Left(error) =>
-          logger.error(s"Error parsing s3 data ($bucket/$key): ${error.getMessage}", error)
-          Left(s"Error parsing s3 data ($bucket/$key): ${error.getMessage}")
-      }
+      Right[String,VersionedS3Data](VersionedS3Data(result, version))
 
     } catch {
       case NonFatal(e) =>
@@ -51,16 +35,16 @@ object S3 extends StrictLogging {
     }
   }
 
-  def putJson(bucket: String, key: String, data: VersionedS3Json)(implicit ec: ExecutionContext): Future[Either[String,Unit]] = Future {
+  def put(bucket: String, key: String, data: VersionedS3Data)(implicit ec: ExecutionContext): Future[Either[String,Unit]] = Future {
     try {
       val currentVersion = s3Client.getObject(bucket, key).getObjectMetadata.getVersionId
 
       if (currentVersion == data.version) {
-        println(s"I would be sending: $data")
+        println(s"I would be sending:")
+        println(data)
         //s3Client.putObject(bucket, key, data.value)
         Right[String, Unit] { () }
       } else {
-        logger.warn(s"Cannot update S3 object $bucket/$key because latest version (${data.version}) does not match current version ($currentVersion)")
         Left(s"Cannot update S3 object $bucket/$key because latest version does not match")
       }
 
