de2f61a63f241e75a7e903612e6a27c848fe7cd8
tomrf1 <tom.forbes@theguardian.com>
2019-01-17 09:24:32 +0000

    Remove serverside data model + form

--- app/controllers/SupportFrontend.scala
+++ app/controllers/SupportFrontend.scala
@@ -1,74 +1,37 @@
 package controllers
 
 import com.gu.googleauth.AuthAction
-import io.circe.{Decoder, Encoder}
-import models._
+import com.gu.googleauth.AuthAction.UserIdentityRequest
+import com.typesafe.scalalogging.StrictLogging
+import io.circe.generic.auto._
 import play.api.libs.circe.Circe
 import play.api.mvc.{AbstractController, AnyContent, ControllerComponents}
-import services.{S3, VersionedS3Data}
-import io.circe.generic.auto._
-import io.circe.syntax._
-import io.circe.parser._
-import play.api.data.Form
-import play.api.data.Forms._
-import play.api.data.Forms.mapping
-import play.api.i18n.Lang
+import services.{S3, VersionedS3Json}
 
-import scala.concurrent.{ExecutionContext, Future}
-
-case class VersionedSettings(settings: SupportFrontendSettings, version: String)
-
-case object VersionedSettings {
-  def fromS3(s3Data: VersionedS3Data): Either[io.circe.Error, VersionedSettings] = {
-    println(s3Data.value)
-
-    decode[SupportFrontendSettings](s3Data.value).map { settings =>
-      VersionedSettings(settings, s3Data.version)
-    }
-  }
-
-  def toS3(versionedSettings: VersionedSettings): VersionedS3Data =
-    VersionedS3Data(versionedSettings.settings.asJson.spaces2, versionedSettings.version)
-
-  val form: Form[VersionedSettings] = Form(
-    mapping(
-      "switches" -> SupportFrontendSwitches.supportFrontendSettingsMapping,
-      "version" -> text
-    )(VersionedSwitches.apply)(VersionedSwitches.unapply)
-  )
-}
+import scala.concurrent.ExecutionContext
 
 class SupportFrontend(authAction: AuthAction[AnyContent], components: ControllerComponents, stage: String)(implicit ec: ExecutionContext)
-  extends AbstractController(components) with Circe {
+  extends AbstractController(components) with Circe with StrictLogging {
 
   private val bucket = "support-frontend-admin-console"
   private val switchesKey = s"$stage/settings.json"
 
-  private implicit val messages = messagesApi.preferred(Seq(Lang.defaultLang))
+  /**
+    * Config is returned as JSON - so the model is only defined in the client code.
+    * We rely on client-side validation and S3 versioning to prevent bad data being written to S3.
+    */
 
   def getSwitches = authAction.async {
-    S3.get(bucket, switchesKey).map {
-      case Right(s3Data) =>
-        VersionedSettings.fromS3(s3Data) match {
-          case Right(settings) =>
-            Ok(views.html.switches(settings))
-
-          case Left(e) =>
-            println(s"Error: $e")
-            InternalServerError(e.getMessage)
-        }
+    S3.getJson(bucket, switchesKey).map {
+      case Right(s3Data) => Ok(VersionedS3Json.toJson(s3Data))
       case Left(error) => InternalServerError(error)
     }
   }
 
-
-  def setSwitches = authAction.async { request =>
-    println(s"body: ${request.body.asFormUrlEncoded}")
-    request.body.asFormUrlEncoded.map(f => f)
-    Future.successful(Ok(""))
-//    S3.put(bucket, switchesKey, VersionedSettings.toS3(request.body)).map {
-//      case Right(_) => Ok("")
-//      case Left(error) => InternalServerError(error)
-//    }
+  def setSwitches = authAction.async(circe.json[VersionedS3Json]) { request: UserIdentityRequest[VersionedS3Json] =>
+    S3.putJson(bucket, switchesKey, request.body).map {
+      case Right(_) => Ok("")
+      case Left(error) => InternalServerError(error)
+    }
   }
 }
--- app/services/S3.scala
+++ app/services/S3.scala
@@ -2,11 +2,23 @@ package services
 
 import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
 import com.typesafe.scalalogging.StrictLogging
+import io.circe.{Json, JsonObject}
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.control.NonFatal
 
-case class VersionedS3Data(value: String, version: String)
+import io.circe.parser.parse
+
+case class VersionedS3Json(value: Json, version: String)
+
+object VersionedS3Json {
+  def toJson(versionedS3Data: VersionedS3Json): Json = Json.fromJsonObject {
+    JsonObject(
+      "version" -> Json.fromString(versionedS3Data.version),
+      "value" -> versionedS3Data.value
+    )
+  }
+}
 
 object S3 extends StrictLogging {
   private val s3Client: AmazonS3 = AmazonS3ClientBuilder
@@ -15,18 +27,22 @@ object S3 extends StrictLogging {
     .withCredentials(Aws.credentialsProvider)
     .build()
 
-  def get(bucket: String, key: String)(implicit ec: ExecutionContext): Future[Either[String,VersionedS3Data]] = Future {
+  def getJson(bucket: String, key: String)(implicit ec: ExecutionContext): Future[Either[String,VersionedS3Json]] = Future {
     try {
       val s3Object = s3Client.getObject(bucket, key)
 
       val version = s3Object.getObjectMetadata.getVersionId
-      println(s"version: $version")
 
       val stream = s3Object.getObjectContent
       val result = scala.io.Source.fromInputStream(stream).mkString
       stream.close()
 
-      Right[String,VersionedS3Data](VersionedS3Data(result, version))
+      parse(result) match {
+        case Right(json) => Right[String,VersionedS3Json](VersionedS3Json(json, version))
+        case Left(error) =>
+          logger.error(s"Error parsing s3 data ($bucket/$key): ${error.getMessage}", error)
+          Left(s"Error parsing s3 data ($bucket/$key): ${error.getMessage}")
+      }
 
     } catch {
       case NonFatal(e) =>
@@ -35,16 +51,16 @@ object S3 extends StrictLogging {
     }
   }
 
-  def put(bucket: String, key: String, data: VersionedS3Data)(implicit ec: ExecutionContext): Future[Either[String,Unit]] = Future {
+  def putJson(bucket: String, key: String, data: VersionedS3Json)(implicit ec: ExecutionContext): Future[Either[String,Unit]] = Future {
     try {
       val currentVersion = s3Client.getObject(bucket, key).getObjectMetadata.getVersionId
 
       if (currentVersion == data.version) {
-        println(s"I would be sending:")
-        println(data)
+        println(s"I would be sending: $data")
         //s3Client.putObject(bucket, key, data.value)
         Right[String, Unit] { () }
       } else {
+        logger.warn(s"Cannot update S3 object $bucket/$key because latest version (${data.version}) does not match current version ($currentVersion)")
         Left(s"Cannot update S3 object $bucket/$key because latest version does not match")
       }
 
