d4ce171e9f9a400e98ec46e7cb0a7314f13166dd
Joseph Smith <joseph.smith@guardian.co.uk>
2019-01-17 18:48:28 +0000

    Add material UI dependencies

--- public/src/main.tsx
+++ public/src/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 
 const Index = () => <h2>Home</h2>;
 const Switches = () => <h2>Switches</h2>;
