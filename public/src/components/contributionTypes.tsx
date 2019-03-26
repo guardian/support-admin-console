f65f62d8c9f0777df001e417e23476e40b0af2b9
Tom Forbes <tom.forbes@theguardian.com>
2019-03-26 16:24:30 +0000

    new form

---
+++ public/src/components/contributionTypes.tsx
@@ -0,0 +1,271 @@
+import React from 'react';
+import update from 'immutability-helper';
+
+import { Theme, withStyles, createStyles, WithStyles } from '@material-ui/core/styles';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+import RefreshIcon from '@material-ui/icons/Refresh';
+
+enum ContributionType {
+  ONE_OFF = 'ONE_OFF',
+  MONTHLY = 'MONTHLY',
+  ANNUAL = 'ANNUAL'
+}
+
+interface ContributionTypeSetting {
+  contributionType: ContributionType,
+  isDefault?: boolean
+}
+
+interface ContributionTypes {
+  GBPCountries: ContributionTypeSetting[],
+  UnitedStates: ContributionTypeSetting[],
+  EURCountries: ContributionTypeSetting[],
+  International: ContributionTypeSetting[],
+  Canada: ContributionTypeSetting[],
+  AUDCountries: ContributionTypeSetting[],
+  NZDCountries: ContributionTypeSetting[]
+}
+
+interface DataFromServer {
+  value: ContributionTypes,
+  version: string,
+}
+
+const allContributionTypes = [
+  {contributionType: ContributionType.ONE_OFF, label: 'One-off'},
+  {contributionType: ContributionType.MONTHLY, label: 'Monthly'},
+  {contributionType: ContributionType.ANNUAL, label: 'Annual'}
+];
+
+const styles = ({ palette, spacing, mixins }: Theme) => createStyles({
+  form: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  button: {
+    marginRight: spacing.unit * 2,
+  },
+  buttons: {
+    marginTop: spacing.unit * 2,
+    marginLeft: spacing.unit * 4
+  },
+  region: {
+    paddingRight: spacing.unit * 4,
+    paddingLeft: spacing.unit * 4,
+    borderBottom: `1px solid ${palette.grey['300']}`,
+    marginBottom: spacing.unit * 4,
+    flexDirection: 'row'
+  },
+  contributionTypes: {
+    flexDirection: 'column',
+    display: 'flex',
+    borderRight: `1px solid ${palette.grey['300']}`,
+    marginRight: spacing.unit * 4
+  },
+  default: {
+    flexDirection: 'column',
+    display: 'flex'
+  },
+  label: {
+    marginTop: spacing.unit * 4
+  }
+});
+
+interface Props extends WithStyles<typeof styles> {}
+
+class ContributionTypesComponent extends React.Component<Props, ContributionTypes> {
+  state: ContributionTypes;
+  previousStateFromServer: DataFromServer | null;
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      GBPCountries: [],
+      UnitedStates: [],
+      EURCountries: [],
+      International: [],
+      Canada: [],
+      AUDCountries: [],
+      NZDCountries: []
+    };
+    this.previousStateFromServer = null;
+  }
+
+  componentWillMount(): void {
+    this.fetchStateFromServer();
+  }
+
+  fetchStateFromServer(): void {
+    fetch('/support-frontend/contributionTypes')
+      .then(resp => {
+        if (!resp.ok) {
+          resp.text().then(msg => alert(msg));
+          throw new Error('Could not fetch initial server state');
+        }
+
+        return resp.json();
+      })
+      .then(serverData => {
+        this.previousStateFromServer = serverData;
+        this.setState({
+          ...serverData.value
+        });
+      });
+  }
+
+  saveSwitches = () => {
+    const newState = update(this.previousStateFromServer, {
+      value: { $set: this.state }
+    });
+
+    fetch('/support-frontend/contributionTypes/update', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newState),
+    })
+      .then(resp => {
+        if (!resp.ok) {
+          resp.text().then(msg => alert(msg));
+        }
+        this.fetchStateFromServer();
+      })
+      .catch((resp) => {
+        alert('Error while saving');
+        this.fetchStateFromServer();
+      });
+  };
+
+  addContributionType(current: ContributionTypeSetting[], newSetting: ContributionTypeSetting): ContributionTypeSetting[] {
+    return current.some(c => c.contributionType === newSetting.contributionType) ?
+      current :
+      current.concat([newSetting]);
+  }
+
+  removeContributionType(current: ContributionTypeSetting[], toRemove: ContributionType): ContributionTypeSetting[] {
+    return current.filter(c => c.contributionType !== toRemove);
+  }
+
+  setContributionTypeOnOff(on: boolean, contributionType: ContributionType, region: string): void {
+    const newSettings = on ?
+      this.addContributionType(this.state[region], {contributionType, isDefault: true}) :
+      this.removeContributionType(this.state[region], contributionType);
+
+    this.setState((prevState) => update(prevState, {
+      [region]: { $set: newSettings }
+    }));
+  }
+
+  setContributionTypeDefault(contributionType: ContributionType, region: string): void {
+    const newSettings = this.state[region].map((c: ContributionTypeSetting) => {
+      if (c.contributionType === contributionType) {
+        return { contributionType: c.contributionType, isDefault: true }
+      } else {
+        return { contributionType: c.contributionType }
+      }
+    });
+
+    this.setState((prevState) => update(prevState, {
+      [region]: { $set: newSettings }
+    }));
+  }
+
+  renderOnOffs(settings: ContributionTypeSetting[], region: string): React.ReactNode {
+    return (
+      <div className={this.props.classes.contributionTypes}>
+      <div className={this.props.classes.label}>Enabled</div>
+      {allContributionTypes.map(({contributionType, label}) =>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={settings.some(c => c.contributionType === contributionType)}
+              value={contributionType}
+              onChange={event =>
+                this.setContributionTypeOnOff(event.target.checked, contributionType, region)
+              }
+            />
+          }
+          label={label}
+        />
+      )}
+      </div>
+    );
+  }
+
+  renderDefaultRadios(current: ContributionType, region: string): React.ReactNode {
+    return (
+      <div className={this.props.classes.default}>
+      <div className={this.props.classes.label}>Default</div>
+      <RadioGroup
+        aria-label="Default"
+        name="default"
+        value={current}
+        onChange={event => this.setContributionTypeDefault(event.target.value, region)}
+      >
+        {allContributionTypes.map(({contributionType, label}) =>
+          <FormControlLabel value={contributionType} control={<Radio />} label=''/>
+        )}
+      </RadioGroup>
+    </div>
+    )
+  }
+
+  renderContributionTypesSettings(settings: ContributionTypeSetting[], region: string): React.ReactNode {
+    const { classes } = this.props;
+
+    const getDefault = (): ContributionType => {
+      //array.find() is not available for some reason
+      const idx = settings.findIndex((c: ContributionTypeSetting) =>
+        typeof c.isDefault !== undefined && c.isDefault === true
+      );
+      if (idx >= 0) return settings[idx].contributionType;
+      else return ContributionType.ONE_OFF;
+    };
+
+    return (
+      <div>
+        <FormControl component={'fieldset' as 'div'} className={classes.region}>
+          <FormLabel component={'legend' as 'label'}>{region}</FormLabel>
+
+          {this.renderOnOffs(settings, region)}
+
+          {this.renderDefaultRadios(getDefault(), region)}
+        </FormControl>
+      </div>
+    );
+  }
+
+  render(): React.ReactNode {
+    const { classes } = this.props;
+
+    return (
+      <form className={classes.form}>
+        <div>
+          {Object.entries(this.state).map(([region, settings]) => this.renderContributionTypesSettings(settings, region))}
+        </div>
+
+        <div className={classes.buttons}>
+          <Button variant="contained" onClick={this.saveSwitches} className={classes.button}>
+            <SaveIcon />
+            Save
+          </Button>
+          <Button variant="contained" onClick={() => this.fetchStateFromServer()} className={classes.button}>
+            <RefreshIcon />
+            Refresh
+          </Button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default withStyles(styles)(ContributionTypesComponent);
