b5d444580c673c6ad98f4ddea81d7cba7952c506
TJ Silver <tjsilver@protonmail.com>
2019-08-14 17:59:17 +0100

    add functionality to move epic test list items up/down

--- public/src/components/epicTests/epicTestsList.tsx
+++ public/src/components/epicTests/epicTestsList.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import {
-  List, ListItem, Theme, createStyles, WithStyles, withStyles, Typography
+  List, ListItem, Theme, createStyles, WithStyles, withStyles, Typography, Button
 } from "@material-ui/core";
 import { EpicTest } from './epicTestsForm';
 import EpicTestEditor from './epicTestEditor';
@@ -24,6 +24,17 @@ const styles = ({ palette }: Theme) => createStyles({
   },
   testListAndEditor: {
     display: "flex"
+  },
+  arrowButton: {
+    padding: "1px",
+    margin: 0,
+    width: "20px",
+    height: "20px",
+    borderRadius: "50%",
+    minWidth: "20px"
+  },
+  buttonContainer: {
+    marginLeft: "20px"
   }
 });
 
@@ -75,6 +86,28 @@ class EpicTestsList extends React.Component<Props, EpicTestsListState> {
     })
   }
 
+  moveTestUp = (name: string) => {
+    const newTests = [...this.props.tests];
+    const indexOfName = newTests.findIndex(test => test.name === name);
+    if (indexOfName > 0) {
+      const beforeElement = newTests[indexOfName - 1]
+      newTests[indexOfName-1] = newTests[indexOfName];
+      newTests[indexOfName] = beforeElement;
+    }
+    this.props.onUpdate(newTests);
+  }
+
+  moveTestDown = (name: string) => {
+    const newTests = [...this.props.tests];
+    const indexOfName = newTests.findIndex(test => test.name === name);
+    if (indexOfName < newTests.length - 1) {
+      const afterElement = newTests[indexOfName + 1]
+      newTests[indexOfName + 1] = newTests[indexOfName];
+      newTests[indexOfName] = afterElement;
+    }
+    this.props.onUpdate(newTests);
+  }
+
   render(): React.ReactNode {
     const { classes } = this.props;
 
@@ -84,7 +117,7 @@ class EpicTestsList extends React.Component<Props, EpicTestsListState> {
 
         <div className={classes.testListAndEditor}>
           <List className={classes.testsList} component="nav">
-            {this.props.tests.map(test => {
+            {this.props.tests.map((test, index) => {
               const classNames = this.state.selectedTestName === test.name ?
                 `${classes.test} ${classes.selectedTest}`
                 : classes.test;
@@ -96,7 +129,11 @@ class EpicTestsList extends React.Component<Props, EpicTestsListState> {
                   onClick={this.onTestSelected}
                   key={test.name}
                 >
-                  <Typography>{test.name}</Typography>
+                 <Typography>{test.name}</Typography>
+                 <div className={classes.buttonContainer}>
+                    {index > 0 ? <Button className={classes.arrowButton} variant={'outlined'} onClick={() => this.moveTestUp(test.name)}>↑</Button> : null}
+                    {index <  this.props.tests.length - 1 ? <Button className={classes.arrowButton} variant={'outlined'} onClick={() => this.moveTestDown(test.name)}>↓</Button> : null}
+                  </div>
                 </ListItem>
               )
             })}
