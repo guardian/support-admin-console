082ee51a7f3d812c2fd0e13244b3cd02d24edc61
Tom Forbes <tom.forbes@theguardian.com>
2019-09-02 09:32:37 +0100

    Share helpers

--- public/src/utils/models.tsx
+++ public/src/utils/models.tsx
@@ -13,3 +13,11 @@ export enum Region {
   AUDCountries = 'AUDCountries',
   NZDCountries = 'NZDCountries'
 }
+
+export function isRegion(s: string): s is Region {
+  return Object.values(Region).includes(s as Region)
+}
+
+export function isContributionType(s: string): s is ContributionType {
+  return Object.values(ContributionType).includes(s as ContributionType)
+}
