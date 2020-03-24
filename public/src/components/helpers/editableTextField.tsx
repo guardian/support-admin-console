34821d6cfc5cdabf7e89bcd3dea0d0b71b965985
TJ Silver <tjsilver@protonmail.com>
2020-03-24 16:48:34 +0000

    remove button and restyle editable text field

--- public/src/components/helpers/editableTextField.tsx
+++ public/src/components/helpers/editableTextField.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import {createStyles, Theme, withStyles, WithStyles, Typography, FormControl, InputLabel} from "@material-ui/core";
-import Button from "@material-ui/core/Button";
+import {createStyles, Theme, withStyles, WithStyles, FormControl} from "@material-ui/core";
 import TextField from '@material-ui/core/TextField';
-import EditIcon from '@material-ui/icons/Edit';
-import SaveIcon from '@material-ui/icons/Save';
 
 const styles = ({ typography, spacing }: Theme) => createStyles({
   container: {
@@ -28,9 +25,6 @@ const styles = ({ typography, spacing }: Theme) => createStyles({
     fontSize: typography.pxToRem(22),
     fontWeight: typography.fontWeightMedium,
     color: "black"
-  },
-  textField: {
-    marginTop: "20px"
   },
   button: {
     marginTop: "30px",
@@ -59,14 +53,12 @@ interface EditableTextFieldProps extends WithStyles<typeof styles> {
 }
 
 interface EditableTextFieldState {
-  fieldEditMode: boolean,
   currentText: string
 }
 
 class EditableTextField extends React.Component<EditableTextFieldProps, EditableTextFieldState> {
 
   state: EditableTextFieldState =  {
-    fieldEditMode: this.props.startInEditMode || false,
     currentText: this.props.text
   };
 
@@ -80,7 +72,6 @@ class EditableTextField extends React.Component<EditableTextFieldProps, Editable
   componentDidUpdate(prevProps: EditableTextFieldProps) {
     if (prevProps.text !== this.props.text) {
       this.setState({
-        fieldEditMode: false,
         currentText: this.props.text
       })
     }
@@ -95,40 +86,16 @@ class EditableTextField extends React.Component<EditableTextFieldProps, Editable
 
     this.setState({
       currentText: newValue
-    })
+    });
   };
 
-  onClickButton = (): void => {
-    if (this.state.fieldEditMode) {
-      if (this.props.validation) {
-        this.props.validation.onChange(this.isValid());
-      }
-
-      this.props.onSubmit(this.state.currentText);
-
-      this.setState({
-        fieldEditMode: false
-      });
-    } else {
-      this.setState({
-        fieldEditMode: true
-      });
+  onExitField = (event: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+    if (this.props.validation) {
+      this.props.validation.onChange(this.isValid());
     }
-  };
 
-  showButton = (): React.ReactFragment | null => {
-    const {classes} = this.props;
-    return this.props.editEnabled && (
-      <Button
-      className={classes.button}
-      type="submit"
-      variant="contained"
-      color="primary"
-      onClick={this.onClickButton}>
-        {this.state.fieldEditMode ? <SaveIcon /> : <EditIcon />}
-      </Button>
-    )
-  }
+    this.props.onSubmit(this.state.currentText);
+  };
 
   render(): React.ReactNode {
     const {classes} = this.props;
@@ -143,30 +110,26 @@ class EditableTextField extends React.Component<EditableTextFieldProps, Editable
             required={this.props.required}
             className={classes.formControl}
           >
-            <InputLabel
-              className={classes.label}
-              shrink
-            >
-              {this.props.label}
-            </InputLabel>
             <TextField
-              className={classes.textField}
+              label={this.props.label}
+              variant={'outlined'}
+              required={this.props.required}
               multiline={this.props.textarea}
               fullWidth
               name={this.props.label}
-              disabled={!this.state.fieldEditMode}
+              disabled={!this.props.editEnabled}
               value={this.state.currentText}
               onChange={this.onChange}
-              helperText={this.props.helperText}
+              helperText={this.props.helperText} // TODO: display error messages here
               autoFocus={this.props.autoFocus}
               error={this.props.validation ? !this.isValid() : false}
+              onBlur={this.onExitField}
             />
           </FormControl>
-         {this.showButton()}
         </div>
-        {error && (
+        {/* {error && (
           <Typography color={'error'} variant={'body2'}>{error}</Typography>
-        )}
+        )} */}
       </>
 
     )
