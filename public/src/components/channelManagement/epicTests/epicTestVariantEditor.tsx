03d70c6ef2f0644f7d9df391f30c430721069441
Tom Pretty <tompretty2@gmail.com>
2020-10-06 10:12:46 +0100

    Highlighted text only shows up for non liveblogs

--- public/src/components/channelManagement/epicTests/epicTestVariantEditor.tsx
+++ public/src/components/channelManagement/epicTests/epicTestVariantEditor.tsx
@@ -162,18 +162,20 @@ class EpicTestVariantEditor extends React.Component<Props, State> {
           fullWidth
         />
 
-        <EditableTextField
-          text={variant.highlightedText || ''}
-          onSubmit={this.onOptionalTextChange('highlightedText')}
-          label="Highlighted text"
-          helperText="Final sentence, highlighted in yellow"
-          editEnabled={this.props.editMode}
-          validation={{
-            getError: (value: string): string | null => getInvalidTemplateError(value),
-            onChange: onFieldValidationChange(this)('highlightedText'),
-          }}
-          fullWidth
-        />
+        {!this.props.isLiveblog && (
+          <EditableTextField
+            text={variant.highlightedText || ''}
+            onSubmit={this.onOptionalTextChange('highlightedText')}
+            label="Highlighted text"
+            helperText="Final sentence, highlighted in yellow"
+            editEnabled={this.props.editMode}
+            validation={{
+              getError: (value: string): string | null => getInvalidTemplateError(value),
+              onChange: onFieldValidationChange(this)('highlightedText'),
+            }}
+            fullWidth
+          />
+        )}
         <div className={classes.ctaContainer}>
           <span className={classes.label}>Buttons</span>
           <CtaEditor
