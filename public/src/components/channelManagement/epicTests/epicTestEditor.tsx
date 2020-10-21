03fb12d924e6aad5fe10a86edbac64f1e76d53d4
Tom Pretty <tompretty2@gmail.com>
2020-10-21 08:32:56 +0100

    Refactor EpicTestEditor into a function component

--- public/src/components/channelManagement/epicTests/epicTestEditor.tsx
+++ public/src/components/channelManagement/epicTests/epicTestEditor.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Region } from '../../../utils/models';
 import { EpicTest, EpicVariant, MaxEpicViews } from './epicTestsForm';
-import { ArticlesViewedSettings, TestEditorState, UserCohort, defaultCta } from '../helpers/shared';
+import { ArticlesViewedSettings, UserCohort, defaultCta } from '../helpers/shared';
 import {
   createStyles,
   FormControlLabel,
@@ -20,7 +20,6 @@ import TestEditorActionButtons from '../testEditorActionButtons';
 import EpicTestVariantEditor from './epicTestVariantEditor';
 import EpicTestTargetContentEditor from './epicTestTargetContentEditor';
 import MaxEpicViewsEditor from './maxEpicViewsEditor';
-import { onFieldValidationChange } from '../helpers/validation';
 import { defaultArticlesViewedSettings } from '../articlesViewedEditor';
 import { articleCountTemplate, countryNameTemplate } from '../helpers/copyTemplates';
 
@@ -89,16 +88,26 @@ interface EpicTestEditorProps extends WithStyles<typeof styles> {
   createTest: (newTest: EpicTest) => void;
 }
 
-class EpicTestEditor extends React.Component<EpicTestEditorProps, TestEditorState> {
-  state: TestEditorState = {
-    validationStatus: {},
+const EpicTestEditor: React.FC<EpicTestEditorProps> = ({
+  classes,
+  test,
+  isLiveblog,
+  onChange,
+  editMode,
+  onDelete,
+  onArchive,
+  onSelectedTestName,
+  isDeleted,
+  isArchived,
+  testNames,
+  testNicknames,
+  createTest,
+}: EpicTestEditorProps) => {
+  const isEditable = (): boolean => {
+    return editMode && !isDeleted && !isArchived;
   };
 
-  isEditable = (): boolean => {
-    return this.props.editMode && !this.props.isDeleted && !this.props.isArchived;
-  };
-
-  getArticlesViewedSettings = (test: EpicTest): ArticlesViewedSettings | undefined => {
+  const getArticlesViewedSettings = (test: EpicTest): ArticlesViewedSettings | undefined => {
     if (!!test.articlesViewedSettings) {
       return test.articlesViewedSettings;
     }
@@ -108,58 +117,44 @@ class EpicTestEditor extends React.Component<EpicTestEditorProps, TestEditorStat
     return undefined;
   };
 
-  updateTest = (update: (test: EpicTest) => EpicTest): void => {
-    if (this.props.test) {
-      const updatedTest = update(this.props.test);
+  const updateTest = (update: (test: EpicTest) => EpicTest): void => {
+    if (test) {
+      const updatedTest = update(test);
 
-      this.props.onChange({
+      onChange({
         ...updatedTest,
         // To save dotcom from having to work this out
         hasCountryName: copyHasTemplate(updatedTest, countryNameTemplate),
-        articlesViewedSettings: this.getArticlesViewedSettings(updatedTest),
+        articlesViewedSettings: getArticlesViewedSettings(updatedTest),
       });
     }
   };
 
-  copyTest = (newTestName: string, newTestNickname: string): void => {
-    if (this.props.test) {
-      this.props.onSelectedTestName(newTestName);
-      const newTest: EpicTest = {
-        ...this.props.test,
-        name: newTestName,
-        nickname: newTestNickname,
-        isOn: false,
-      };
-      this.props.createTest(newTest);
+  const onVariantsChange = (updatedVariantList: EpicVariant[]): void => {
+    if (test) {
+      updateTest(test => ({ ...test, variants: updatedVariantList }));
     }
   };
 
-  onVariantsChange = (updatedVariantList: EpicVariant[]): void => {
-    if (this.props.test) {
-      this.updateTest(test => ({ ...test, variants: updatedVariantList }));
-    }
-  };
-
-  onVariantChange = (updatedVariant: EpicVariant): void => {
-    if (this.props.test) {
-      const updatedVariantList = this.props.test.variants.map(variant =>
+  const onVariantChange = (updatedVariant: EpicVariant): void => {
+    if (test) {
+      const updatedVariantList = test.variants.map(variant =>
         variant.name === updatedVariant.name ? updatedVariant : variant,
       );
-      this.onVariantsChange(updatedVariantList);
+      onVariantsChange(updatedVariantList);
     }
   };
 
-  onVariantDelete = (deletedVariantName: string): void => {
-    if (this.props.test) {
-      const updatedVariantList = this.props.test.variants.filter(
+  const onVariantDelete = (deletedVariantName: string): void => {
+    if (test) {
+      const updatedVariantList = test.variants.filter(
         variant => variant.name !== deletedVariantName,
       );
-      this.onVariantsChange(updatedVariantList);
+      onVariantsChange(updatedVariantList);
     }
   };
 
-  createVariant = (name: string): void => {
-    console.log(name);
+  const createVariant = (name: string): void => {
     const newVariant: EpicVariant = {
       name: name,
       heading: undefined,
@@ -172,201 +167,177 @@ class EpicTestEditor extends React.Component<EpicTestEditorProps, TestEditorStat
       cta: defaultCta,
     };
 
-    if (this.props.test) {
-      this.onVariantsChange([...this.props.test.variants, newVariant]);
-      console.log(this.props.test.variants);
+    if (test) {
+      onVariantsChange([...test.variants, newVariant]);
     }
-
-    // onVariantsListChange([...variants, newVariant]);
-    // onVariantSelected(`${testName}-${name}`);
   };
 
-  onListChange = (fieldName: string) => (updatedString: string): void => {
-    const updatedList = updatedString === '' ? [] : updatedString.split(',');
-    this.updateTest(test => ({ ...test, [fieldName]: updatedList }));
-  };
-
-  onSwitchChange = (fieldName: string) => (event: React.ChangeEvent<HTMLInputElement>): void => {
+  const onSwitchChange = (fieldName: string) => (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ): void => {
     const updatedBool = event.target.checked;
-    this.updateTest(test => ({ ...test, [fieldName]: updatedBool }));
+    updateTest(test => ({ ...test, [fieldName]: updatedBool }));
   };
 
-  onLiveSwitchChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    this.updateTest(test => ({ ...test, isOn: event.target.checked }));
+  const onLiveSwitchChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    updateTest(test => ({ ...test, isOn: event.target.checked }));
   };
 
-  onUserCohortChange = (event: React.ChangeEvent<{}>, value: string): void => {
-    const selectedCohort = value as UserCohort;
-    this.updateTest(test => ({ ...test, userCohort: selectedCohort }));
-  };
-
-  onTargetRegionsChange = (selectedRegions: Region[]): void => {
-    this.updateTest(test => ({ ...test, locations: selectedRegions }));
-  };
-
-  updateTargetSections = (
+  const updateTargetSections = (
     tagIds: string[],
     sections: string[],
     excludedTagIds: string[],
     excludedSections: string[],
   ): void => {
-    this.updateTest(test => ({ ...test, tagIds, sections, excludedTagIds, excludedSections }));
+    updateTest(test => ({ ...test, tagIds, sections, excludedTagIds, excludedSections }));
   };
 
-  onRegionsChange = (updatedRegions: Region[]): void => {
-    this.updateTest(test => ({ ...test, locations: updatedRegions }));
+  const onRegionsChange = (updatedRegions: Region[]): void => {
+    updateTest(test => ({ ...test, locations: updatedRegions }));
   };
 
-  onCohortChange = (updatedCohort: UserCohort): void => {
-    this.updateTest(test => ({ ...test, userCohort: updatedCohort }));
+  const onCohortChange = (updatedCohort: UserCohort): void => {
+    updateTest(test => ({ ...test, userCohort: updatedCohort }));
   };
 
-  onArticlesViewedSettingsChange = (
+  const onArticlesViewedSettingsChange = (
     updatedArticlesViewedSettings?: ArticlesViewedSettings,
   ): void => {
-    this.updateTest(test => ({
+    updateTest(test => ({
       ...test,
       articlesViewedSettings: updatedArticlesViewedSettings,
     }));
   };
 
-  onCopy = (name: string, nickname: string): void => {
-    this.props.onSelectedTestName(name);
-    this.props.createTest({ ...this.props.test, name: name, nickname: nickname, isOn: false });
+  const onCopy = (name: string, nickname: string): void => {
+    onSelectedTestName(name);
+    createTest({ ...test, name: name, nickname: nickname, isOn: false });
   };
 
-  renderEditor = (test: EpicTest): React.ReactNode | undefined => {
-    const { classes } = this.props;
+  const variantEditors = test.variants.map(variant => (
+    <EpicTestVariantEditor
+      key={variant.name}
+      variant={variant}
+      isLiveblog={isLiveblog}
+      editMode={editMode}
+      onVariantChange={onVariantChange}
+      onDelete={(): void => onVariantDelete(variant.name)}
+      onValidationChange={isValid => console.log(isValid)}
+    />
+  ));
 
-    const variantEditors = test.variants.map(variant => (
-      <EpicTestVariantEditor
-        key={variant.name}
-        variant={variant}
-        isLiveblog={test.isLiveBlog}
-        editMode={this.props.editMode}
-        onVariantChange={this.onVariantChange}
-        onDelete={(): void => this.onVariantDelete(variant.name)}
-        onValidationChange={onFieldValidationChange(this)(`variant-${variant.name}`)}
-      />
-    ));
+  return (
+    <div className={classes.container}>
+      <div className={classes.headerAndSwitchContainer}>
+        <TestEditorHeader name={test.name} nickname={test.nickname} />
 
-    return (
-      <div className={classes.container}>
-        <div className={classes.headerAndSwitchContainer}>
-          <TestEditorHeader name={test.name} nickname={test.nickname} />
+        <TestEditorLiveSwitch
+          isChecked={test.isOn}
+          isDisabled={!isEditable()}
+          onChange={onLiveSwitchChange}
+        />
+      </div>
 
-          <TestEditorLiveSwitch
-            isChecked={test.isOn}
-            isDisabled={!this.isEditable()}
-            onChange={this.onLiveSwitchChange}
-          />
-        </div>
-
-        <div className={classes.sectionContainer}>
-          <Typography variant={'h3'} className={classes.sectionHeader}>
-            Variants
-          </Typography>
-          <div>
-            <TestVariantsEditor<EpicVariant>
-              variants={test.variants}
-              testName={test.name}
-              editMode={this.isEditable()}
-              createVariant={this.createVariant}
-              variantEditors={variantEditors}
-              onVariantDelete={this.onVariantDelete}
-            />
-          </div>
-        </div>
-
-        <div className={classes.sectionContainer}>
-          <Typography variant={'h3'} className={classes.sectionHeader}>
-            Target content
-          </Typography>
-
-          <EpicTestTargetContentEditor
-            tagIds={test.tagIds}
-            sections={test.sections}
-            excludeTagIds={test.excludedTagIds}
-            excludeSections={test.excludedSections}
-            editMode={this.isEditable()}
-            updateTargetContent={this.updateTargetSections}
-          />
-        </div>
-
-        <div className={classes.sectionContainer}>
-          <Typography variant={'h3'} className={classes.sectionHeader}>
-            Target audience
-          </Typography>
-
-          <TestEditorTargetAudienceSelector
-            selectedRegions={test.locations}
-            onRegionsUpdate={this.onRegionsChange}
-            selectedCohort={test.userCohort}
-            onCohortChange={this.onCohortChange}
-            isDisabled={!this.isEditable()}
-          />
-        </div>
-
-        <div className={classes.sectionContainer}>
-          <Typography variant={'h3'} className={classes.sectionHeader}>
-            View frequency settings
-          </Typography>
-
-          <FormControlLabel
-            control={
-              <Switch
-                checked={test.useLocalViewLog}
-                onChange={this.onSwitchChange('useLocalViewLog')}
-                disabled={!this.isEditable()}
-              />
-            }
-            label={`Use private view counter for this test (instead of the global one)`}
-          />
-
-          <MaxEpicViewsEditor
-            test={test}
-            editMode={this.isEditable()}
-            onChange={(alwaysAsk: boolean, maxEpicViews: MaxEpicViews): void =>
-              this.updateTest(test => ({
-                ...test,
-                alwaysAsk,
-                maxViews: maxEpicViews,
-              }))
-            }
-            onValidationChange={onFieldValidationChange(this)('maxViews')}
-          />
-        </div>
-
-        <div className={classes.sectionContainer}>
-          <Typography variant={'h3'} className={classes.sectionHeader}>
-            Article count
-          </Typography>
-
-          <TestEditorArticleCountEditor
-            articlesViewedSettings={test.articlesViewedSettings}
-            onArticlesViewedSettingsChanged={this.onArticlesViewedSettingsChange}
-            onValidationChange={isValid => console.log(isValid)}
-            isDisabled={!this.isEditable()}
-          />
-        </div>
-
-        <div className={classes.buttonsContainer}>
-          <TestEditorActionButtons
-            existingNames={this.props.testNames}
-            existingNicknames={this.props.testNicknames}
-            isDisabled={!this.isEditable()}
-            onArchive={this.props.onArchive}
-            onDelete={this.props.onDelete}
-            onCopy={this.onCopy}
+      <div className={classes.sectionContainer}>
+        <Typography variant={'h3'} className={classes.sectionHeader}>
+          Variants
+        </Typography>
+        <div>
+          <TestVariantsEditor<EpicVariant>
+            variants={test.variants}
+            testName={test.name}
+            editMode={isEditable()}
+            createVariant={createVariant}
+            variantEditors={variantEditors}
+            onVariantDelete={onVariantDelete}
           />
         </div>
       </div>
-    );
-  };
 
-  render(): React.ReactNode {
-    return this.props.test ? this.props.visible && this.renderEditor(this.props.test) : null;
-  }
-}
+      <div className={classes.sectionContainer}>
+        <Typography variant={'h3'} className={classes.sectionHeader}>
+          Target content
+        </Typography>
+
+        <EpicTestTargetContentEditor
+          tagIds={test.tagIds}
+          sections={test.sections}
+          excludeTagIds={test.excludedTagIds}
+          excludeSections={test.excludedSections}
+          editMode={isEditable()}
+          updateTargetContent={updateTargetSections}
+        />
+      </div>
+
+      <div className={classes.sectionContainer}>
+        <Typography variant={'h3'} className={classes.sectionHeader}>
+          Target audience
+        </Typography>
+
+        <TestEditorTargetAudienceSelector
+          selectedRegions={test.locations}
+          onRegionsUpdate={onRegionsChange}
+          selectedCohort={test.userCohort}
+          onCohortChange={onCohortChange}
+          isDisabled={!isEditable()}
+        />
+      </div>
+
+      <div className={classes.sectionContainer}>
+        <Typography variant={'h3'} className={classes.sectionHeader}>
+          View frequency settings
+        </Typography>
+
+        <FormControlLabel
+          control={
+            <Switch
+              checked={test.useLocalViewLog}
+              onChange={onSwitchChange('useLocalViewLog')}
+              disabled={!isEditable()}
+            />
+          }
+          label={`Use private view counter for this test (instead of the global one)`}
+        />
+
+        <MaxEpicViewsEditor
+          test={test}
+          editMode={isEditable()}
+          onChange={(alwaysAsk: boolean, maxEpicViews: MaxEpicViews): void =>
+            updateTest(test => ({
+              ...test,
+              alwaysAsk,
+              maxViews: maxEpicViews,
+            }))
+          }
+          onValidationChange={isValid => console.log(isValid)}
+        />
+      </div>
+
+      <div className={classes.sectionContainer}>
+        <Typography variant={'h3'} className={classes.sectionHeader}>
+          Article count
+        </Typography>
+
+        <TestEditorArticleCountEditor
+          articlesViewedSettings={test.articlesViewedSettings}
+          onArticlesViewedSettingsChanged={onArticlesViewedSettingsChange}
+          onValidationChange={isValid => console.log(isValid)}
+          isDisabled={!isEditable()}
+        />
+      </div>
+
+      <div className={classes.buttonsContainer}>
+        <TestEditorActionButtons
+          existingNames={testNames}
+          existingNicknames={testNicknames}
+          isDisabled={!isEditable()}
+          onArchive={onArchive}
+          onDelete={onDelete}
+          onCopy={onCopy}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default withStyles(styles)(EpicTestEditor);
