bd8904e083721d8ccdb3e7132d9bd93752cac4ac
Tom Forbes <tom.forbes@theguardian.com>
2020-11-11 14:53:10 +0000

    hide for now

--- public/src/components/drawer.tsx
+++ public/src/components/drawer.tsx
@@ -135,18 +135,18 @@ export default function NavDrawer(): React.ReactElement {
             <ListItemText primary="Liveblog Epic" />
           </ListItem>
         </Link>
-        <Link key="Apple News Epic" to="/apple-news-epic-tests" className={classes.link}>
-          <ListItem className={classes.listItem} button key="Apple News Epic">
-            <ListItemText primary="Apple News Epic" />
-            <img className={classes.icon} src="assets/images/apple-news-icon.png" />
-          </ListItem>
-        </Link>
-        <Link key="AMP Epic" to="/amp-epic-tests" className={classes.link}>
-          <ListItem className={classes.listItem} button key="AMP Epic">
-            <ListItemText primary="AMP Epic" />
-            <img className={classes.icon} src="assets/images/amp-icon.png" />
-          </ListItem>
-        </Link>
+        {/*<Link key="Apple News Epic" to="/apple-news-epic-tests" className={classes.link}>*/}
+          {/*<ListItem className={classes.listItem} button key="Apple News Epic">*/}
+            {/*<ListItemText primary="Apple News Epic" />*/}
+            {/*<img className={classes.icon} src="assets/images/apple-news-icon.png" />*/}
+          {/*</ListItem>*/}
+        {/*</Link>*/}
+        {/*<Link key="AMP Epic" to="/amp-epic-tests" className={classes.link}>*/}
+          {/*<ListItem className={classes.listItem} button key="AMP Epic">*/}
+            {/*<ListItemText primary="AMP Epic" />*/}
+            {/*<img className={classes.icon} src="assets/images/amp-icon.png" />*/}
+          {/*</ListItem>*/}
+        {/*</Link>*/}
         <Link key="Banner1" to="/banner-tests" className={classes.link}>
           <ListItem className={classes.listItem} button key="Banner1">
             <ListItemText primary="Banner 1" />
