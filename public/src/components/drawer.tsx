325b96d1b510068b8db53ac2017ec40aca1e1635
Tom Pretty <tompretty2@gmail.com>
2021-01-20 12:00:43 +0000

    Add link to drawer

--- public/src/components/drawer.tsx
+++ public/src/components/drawer.tsx
@@ -130,6 +130,11 @@ export default function NavDrawer(): React.ReactElement {
             <ListItemText primary="Epic" />
           </ListItem>
         </Link>
+        <Link key="Epic Holdback" to="/epic-holdback-tests" className={classes.link}>
+          <ListItem className={classes.listItem} button key="Epic Holdback">
+            <ListItemText primary="Epic Holdback" />
+          </ListItem>
+        </Link>
         <Link key="Liveblog Epic" to="/liveblog-epic-tests" className={classes.link}>
           <ListItem className={classes.listItem} button key="Liveblog Epic">
             <ListItemText primary="Liveblog Epic" />
