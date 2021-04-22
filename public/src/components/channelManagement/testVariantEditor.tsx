e40012467b18f17b66685f04858b3f9f838c6ee2
Tom Pretty <tompretty2@gmail.com>
2021-04-22 13:12:51 +0100

    Remove split

--- public/src/components/channelManagement/testVariantEditor.tsx
+++ public/src/components/channelManagement/testVariantEditor.tsx
@@ -46,18 +46,11 @@ function TestVariantEditor({
       >
         <Tab label="Form" />
         <Tab label="Preview" />
-        <Tab label="Split" />
       </Tabs>
 
       <div>
         {value === 0 && variantEditor}
         {value === 1 && variantPreview}
-        {value === 2 && (
-          <div className={classes.splitContainer}>
-            <div className={classes.splitContent}>{variantEditor}</div>
-            <div className={classes.splitContent}>{variantPreview}</div>
-          </div>
-        )}
       </div>
     </div>
   );
