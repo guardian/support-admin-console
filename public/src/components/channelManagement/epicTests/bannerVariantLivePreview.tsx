4f39c4bfb8d316e094156dd90d7b450d87d84bc4
Tom Pretty <tompretty2@gmail.com>
2021-04-22 10:32:27 +0100

    wip

--- public/src/components/channelManagement/epicTests/bannerVariantLivePreview.tsx
+++ public/src/components/channelManagement/epicTests/bannerVariantLivePreview.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from 'react';
 import { Theme, makeStyles } from '@material-ui/core';
 import * as emotion from 'emotion';
 import * as emotionCore from '@emotion/core';
@@ -6,9 +6,8 @@ import * as emotionTheming from 'emotion-theming';
 import IconButton from '@material-ui/core/IconButton';
 import Drawer from '@material-ui/core/Drawer';
 import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import {BannerTemplate, BannerVariant} from "../../../models/banner";
-import {Cta} from "../helpers/shared";
-
+import { BannerTemplate, BannerVariant } from '../../../models/banner';
+import { Cta } from '../helpers/shared';
 
 interface BannerProps {
   tracking: {
@@ -32,62 +31,62 @@ interface BannerProps {
     heading?: string;
     highlightedText?: string;
     cta?: Cta;
-  }
+  };
 }
 
 const anchor = 'right';
 
 const buildProps = (variant: BannerVariant): BannerProps => ({
   tracking: {
-    ophanPageId: "ophanPageId",
-    platformId: "GUARDIAN_WEB",
-    clientName: "frontend",
-    referrerUrl: "https://www.theguardian.com/",
-    abTestName: "abTestName",
+    ophanPageId: 'ophanPageId',
+    platformId: 'GUARDIAN_WEB',
+    clientName: 'frontend',
+    referrerUrl: 'https://www.theguardian.com/',
+    abTestName: 'abTestName',
     abTestVariant: variant.name,
-    campaignCode: "campaignCode",
-    campaignId: "",
-    componentType: "ACQUISITIONS_ENGAGEMENT_BANNER",
-    products: []
+    campaignCode: 'campaignCode',
+    campaignId: '',
+    componentType: 'ACQUISITIONS_ENGAGEMENT_BANNER',
+    products: [],
   },
-  bannerChannel: "contributions",
+  bannerChannel: 'contributions',
   isSupporter: false,
   content: {
-    messageText: variant.body,
+    messageText: variant.body || '',
     heading: variant.heading,
     highlightedText: variant.highlightedText,
-    cta: variant.cta
+    cta: variant.cta,
   },
-  countryCode: "GB",
-  numArticles: 13
+  countryCode: 'GB',
+  numArticles: 13,
 });
 
 const bannerModules = {
   [BannerTemplate.GuardianWeeklyBanner]: {
     path: 'guardian-weekly-banner.js',
-    name: 'GuardianWeeklyBanner'
+    name: 'GuardianWeeklyBanner',
   },
   [BannerTemplate.DigitalSubscriptionsBanner]: {
     path: 'digital-subscriptions-banner.js',
-    name: 'DigitalSubscriptionsBanner'
+    name: 'DigitalSubscriptionsBanner',
   },
   [BannerTemplate.ContributionsBanner]: {
     path: 'contributions-banner.js',
-    name: 'ContributionsBanner'
-  }
+    name: 'ContributionsBanner',
+  },
 };
 
-const useStyles = makeStyles(({ palette, spacing }: Theme) => ({
+const useStyles = makeStyles(({ palette }: Theme) => ({
   container: {
-    position: "fixed",
-    top: "50vh",
-    right: "10px",
+    position: 'fixed',
+    top: '50vh',
+    right: '10px',
   },
   drawer: {
     height: 'auto',
     bottom: 0,
     top: 'auto',
-    width: '100%'
+    width: '100%',
   },
   icon: {
     width: '40px',
@@ -96,7 +95,7 @@ const useStyles = makeStyles(({ palette, spacing }: Theme) => ({
     borderRadius: '50%',
     color: 'white',
     cursor: 'pointer',
-  }
+  },
 }));
 
 interface BannerVariantLivePreviewProps {
@@ -104,7 +103,7 @@ interface BannerVariantLivePreviewProps {
 }
 
 const BannerVariantLivePreview: React.FC<BannerVariantLivePreviewProps> = ({
-  variant
+  variant,
 }: BannerVariantLivePreviewProps) => {
   const classes = useStyles();
 
@@ -112,7 +111,6 @@ const BannerVariantLivePreview: React.FC<BannerVariantLivePreviewProps> = ({
   const [drawerOpen, setDrawerOpen] = useState<boolean>();
 
   useEffect(() => {
-    // @ts-ignore
     window.guardian.automat = {
       react: React,
       preact: React,
@@ -121,44 +119,47 @@ const BannerVariantLivePreview: React.FC<BannerVariantLivePreviewProps> = ({
       emotion,
     };
 
-    // @ts-ignore
-    window.remoteImport(`https://contributions.guardianapis.com/${bannerModules[variant.template].path}`).then(bannerModule => {
-      setBanner(() => bannerModule[bannerModules[variant.template].name]);
-    })
+    window
+      .remoteImport(
+        `https://contributions.guardianapis.com/${bannerModules[variant.template].path}`,
+      )
+      .then(bannerModule => {
+        setBanner(() => bannerModule[bannerModules[variant.template].name]);
+      });
   }, [variant.template]);
 
-  const toggleDrawer = (open: boolean) => (
-    event: React.KeyboardEvent | React.MouseEvent,
-  ): void => {
+  const toggleDrawer = (open: boolean) => (): void => {
     setDrawerOpen(open);
   };
-
 
   const props = buildProps(variant);
 
   return (
     <div className={classes.container}>
-      {
-        Banner && (
-          <React.Fragment key={anchor}>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              onClick={toggleDrawer(true)}
-              edge="start"
-            >
-              <ChevronLeft className={classes.icon}/>
-            </IconButton>
-            <Drawer anchor={anchor} open={drawerOpen} onClose={toggleDrawer(false)} classes={{paper: classes.drawer}}>
-              <div>
-                <Banner {...props}/>
-              </div>
-            </Drawer>
-          </React.Fragment>
-        )
-      }
+      {Banner && (
+        <React.Fragment key={anchor}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleDrawer(true)}
+            edge="start"
+          >
+            <ChevronLeft className={classes.icon} />
+          </IconButton>
+          <Drawer
+            anchor={anchor}
+            open={drawerOpen}
+            onClose={toggleDrawer(false)}
+            classes={{ paper: classes.drawer }}
+          >
+            <div>
+              <Banner {...props} />
+            </div>
+          </Drawer>
+        </React.Fragment>
+      )}
     </div>
-  )
+  );
 };
 
 export default BannerVariantLivePreview;
