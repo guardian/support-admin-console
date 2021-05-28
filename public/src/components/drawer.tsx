6910888c5f9123f9f546dab9e789fd6c3aeda710
Tom Pretty <tompretty2@gmail.com>
2021-05-28 08:30:46 +0100

    Add issue link

--- public/src/components/drawer.tsx
+++ public/src/components/drawer.tsx
@@ -199,9 +199,23 @@ export default function NavDrawer(): React.ReactElement {
           href="https://docs.google.com/document/d/15XeUDHMSt7Urv0I3LyAAhC8kcC2JRWtkiD1xlEVitcE"
           key="Testing FAQ"
           className={classes.link}
+          target="_blank"
+          rel="noopener noreferrer"
         >
           <ListItem className={classes.listItem} button key="Testing FAQ">
             <ListItemText primary="Testing FAQ" />
+          </ListItem>
+        </a>
+
+        <a
+          href="https://forms.gle/Z8Fzn6iw2d9F631n9"
+          key="Report an issue"
+          className={classes.link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <ListItem className={classes.listItem} button key="Report an issue">
+            <ListItemText primary="Report an issue" />
           </ListItem>
         </a>
       </div>
