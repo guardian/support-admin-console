b1a3e249e09985d95795d174d7d6af1eee3d0a03
Tom Forbes <tom.forbes@theguardian.com>
2021-11-09 11:02:50 +0000

    Capi proxy endpoints

---
+++ app/services/CapiService.scala
@@ -0,0 +1,19 @@
+package services
+
+import play.api.libs.ws.WSClient
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class CapiService(apiKey: String, wsClient: WSClient)(implicit val ec: ExecutionContext) {
+  private val url = "https://content.guardianapis.com"
+
+  private def request(path: String, querystring: String): Future[String] =
+    wsClient
+      .url(s"$url/$path?api-key=$apiKey&$querystring")
+      .get
+      .map(response => response.body)
+
+  def getTags(querystring: String): Future[String] = request("tags", querystring)
+
+  def getSections(querystring: String): Future[String] = request("sections", querystring)
+}
