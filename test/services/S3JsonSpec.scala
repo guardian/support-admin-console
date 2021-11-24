109f740c706ff88be9cfe113f9c5ee128f6a3a9d
Tom Forbes <tom.forbes@theguardian.com>
2021-11-24 10:44:13 +0000

    WIP - migrate switches config

--- test/services/S3JsonSpec.scala
+++ test/services/S3JsonSpec.scala
@@ -1,10 +1,10 @@
 package services
 
-import models._
 import org.scalatest.{EitherValues, FlatSpec, Matchers}
 import services.S3Client.{RawVersionedS3Data, S3Action, S3ObjectSettings}
-import io.circe.generic.auto._
 import gnieh.diffson.circe._
+import models.SupportFrontendSwitches.{SupportFrontendSwitches, Switch}
+import models.SwitchState.On
 import zio.{DefaultRuntime, IO}
 
 import scala.concurrent.Await
@@ -17,72 +17,67 @@ class S3JsonSpec extends FlatSpec with Matchers with EitherValues {
   val expectedJson: String =
     """
       |{
-      |      "oneOffPaymentMethods": {
-      |        "stripe": "On",
-      |        "stripeApplePay": "On",
-      |        "stripePaymentRequestButton": "On",
-      |        "payPal": "On",
-      |        "amazonPay": "On"
-      |      },
-      |      "recurringPaymentMethods": {
-      |        "stripe": "On",
-      |        "stripeApplePay": "On",
-      |        "stripePaymentRequestButton": "On",
-      |        "payPal": "On",
-      |        "directDebit": "On",
-      |        "existingCard": "On",
-      |        "existingDirectDebit": "On",
-      |        "sepa": "On"
-      |      },
-      |      "experiments": {
-      |        "newFlow": {
-      |          "name": "newFlow",
-      |          "description": "Redesign of the payment flow UI",
-      |          "state": "On"
-      |        }
-      |      },
-      |      "enableDigitalSubGifting": "Off",
-      |      "useDotcomContactPage": "Off",
-      |      "enableRecaptchaBackend" : "On",
-      |      "enableRecaptchaFrontend" : "On",
-      |      "enableContributionsCampaign" : "On",
-      |      "forceContributionsCampaign" : "On",
-      |      "enableQuantumMetric" : "On"
+      |  "oneOffPaymentMethods" : {
+      |    "stripe" : {
+      |      "state": "On",
+      |      "description": "Stripe"
+      |    },
+      |    "payPal" : {
+      |      "state": "On",
+      |      "description": "PayPal"
+      |    }
+      |  },
+      |  "recurringPaymentMethods" : {
+      |    "stripe" : {
+      |      "state": "On",
+      |      "description": "Stripe"
+      |    },
+      |    "payPal" : {
+      |      "state": "On",
+      |      "description": "PayPal"
+      |    }
+      |  },
+      |  "featureSwitches" : {
+      |    "enableQuantumMetric": {
+      |      "state": "On",
+      |      "description": "Enable Quantum Metric"
+      |    },
+      |    "checkoutPostcodeLookup": {
+      |      "state": "On",
+      |      "description": "Enable external service postcode lookup in checkout form"
+      |    }
+      |  },
+      |  "campaignSwitches": {
+      |    "enableCampaign": {
+      |      "state": "On",
+      |      "description": "Enable contributions campaign"
+      |    },
+      |    "forceAll": {
+      |      "state": "On",
+      |      "description": "Force all users into the campaign"
+      |    }
+      |  }
       |}
     """.stripMargin
 
   val expectedDecoded: VersionedS3Data[SupportFrontendSwitches] = VersionedS3Data(
-    SupportFrontendSwitches(
-      oneOffPaymentMethods = PaymentMethodSwitches(
-        stripe = On,
-        payPal = On,
-        amazonPay = Some(On),
-        directDebit = None,
-        existingCard = None,
-        existingDirectDebit = None,
-        stripeApplePay = On,
-        stripePaymentRequestButton = On,
-        sepa = None
+    Map(
+      "oneOffPaymentMethods" -> Map(
+        "stripe" -> Switch("Stripe", On),
+        "payPal" -> Switch("PayPal", On),
       ),
-      recurringPaymentMethods = PaymentMethodSwitches(
-        stripe = On,
-        payPal = On,
-        amazonPay = None,
-        directDebit = Some(On),
-        existingCard = Some(On),
-        existingDirectDebit = Some(On),
-        stripeApplePay = On,
-        stripePaymentRequestButton = On,
-        sepa = Some(On)
+      "recurringPaymentMethods" -> Map(
+        "stripe" -> Switch("Stripe", On),
+        "payPal" -> Switch("PayPal", On),
       ),
-      experiments = Map("newFlow" -> ExperimentSwitch("newFlow","Redesign of the payment flow UI",On)),
-      enableDigitalSubGifting = Off,
-      useDotcomContactPage = Off,
-      enableRecaptchaBackend = On,
-      enableRecaptchaFrontend = On,
-      enableContributionsCampaign = On,
-      forceContributionsCampaign = On,
-      enableQuantumMetric = On
+      "featureSwitches" -> Map(
+        "enableQuantumMetric" -> Switch("Enable Quantum Metric", On),
+        "checkoutPostcodeLookup" -> Switch("Enable external service postcode lookup in checkout form", On)
+      ),
+      "campaignSwitches" -> Map(
+        "enableCampaign"-> Switch("Enable contributions campaign", On),
+        "forceAll"-> Switch("Force all users into the campaign", On),
+      )
     ),
     version = "v1"
   )
