b33c4d06f1dbb24f839bcb0131e636817f30ac58
Rik Roots <rik.roots@gmail.com>
2022-02-25 15:33:03 +0000

    action suggestions made in PR comments

--- public/src/components/channelManagement/bannerTests/utils/defaults.ts
+++ public/src/components/channelManagement/bannerTests/utils/defaults.ts
@@ -18,8 +18,6 @@ const DEV_AND_CODE_DEFAULT_VARIANT: BannerVariant = {
   template: BannerTemplate.ContributionsBanner,
   bannerContent: {
     heading: 'We chose a different approach. Will you support it?',
-    messageText:
-      'We believe every one of us deserves to read quality, independent, fact-checked news and measured explanation – that’s why we keep Guardian journalism open to all. Our editorial independence has never been so vital. No one sets our agenda, or edits our editor, so we can keep providing independent reporting each and every day. No matter how unpredictable the future feels, we will remain with you. Every contribution, however big or small, makes our work possible – in times of crisis and beyond.',
     paragraphs: [
       'We believe every one of us deserves to read quality, independent, fact-checked news and measured explanation – that’s why we keep Guardian journalism open to all. Our editorial independence has never been so vital. No one sets our agenda, or edits our editor, so we can keep providing independent reporting each and every day. No matter how unpredictable the future feels, we will remain with you. Every contribution, however big or small, makes our work possible – in times of crisis and beyond.',
     ],
@@ -32,7 +30,6 @@ const PROD_DEFAULT_VARIANT: BannerVariant = {
   name: 'CONTROL',
   template: BannerTemplate.ContributionsBanner,
   bannerContent: {
-    messageText: '',
     paragraphs: [],
     highlightedText: 'Support the Guardian from as little as %%CURRENCY_SYMBOL%%1. Thank you.',
     cta: DEFAULT_PRIMARY_CTA,
