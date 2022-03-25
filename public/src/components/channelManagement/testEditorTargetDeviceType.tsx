8c7190e7c05e9369d636ac85b1e0f1be93db7499
Tom Forbes <tom.forbes@theguardian.com>
2022-03-25 15:27:58 +0000

    WIP

--- public/src/components/channelManagement/testEditorTargetDeviceType.tsx
+++ public/src/components/channelManagement/testEditorTargetDeviceType.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
-import { Checkbox, FormControlLabel, FormGroup, Theme, makeStyles } from '@material-ui/core';
+import {FormControlLabel, FormGroup, Radio, RadioGroup} from '@material-ui/core';
 import { DeviceType } from './helpers/shared';
 
-const useStyles = makeStyles(({ spacing }: Theme) => ({
-  indentedContainer: {
-    marginLeft: spacing(3),
-  },
-}));
+interface DeviceTypeRadioProps {
+  deviceType: DeviceType;
+  isDisabled: boolean;
+  onChange: (updatedDeviceType: DeviceType) => void;
+}
+const DeviceTypeRadio: React.FC<DeviceTypeRadioProps> = ({
+  deviceType,
+  onChange,
+  isDisabled,
+}) => <FormControlLabel
+  value={deviceType}
+  key={deviceType}
+  control={<Radio />}
+  label={deviceType}
+  disabled={isDisabled}
+  onChange={() => onChange(deviceType)}
+/>;
 
 interface TestEditorTargetDeviceTypeProps {
   selectedDeviceType: DeviceType;
@@ -18,60 +30,13 @@ const TestEditorTargetDeviceType: React.FC<TestEditorTargetDeviceTypeProps> = ({
   selectedDeviceType,
   onChange,
   isDisabled,
-}: TestEditorTargetDeviceTypeProps) => {
-  const classes = useStyles();
-
-  const onSelect = (deviceType: DeviceType) => (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ): void => {
-    if (event.target.checked) {
-      // It's impossible to have had neither checked, so it must now be both
-      onChange('All');
-    } else if (selectedDeviceType === 'All') {
-      if (deviceType === 'Desktop') {
-        onChange('Mobile');
-      } else if (deviceType === 'Mobile') {
-        onChange('Desktop');
-      }
-    }
-  };
-
-  return (
+}: TestEditorTargetDeviceTypeProps) =>
     <FormGroup>
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={selectedDeviceType === 'All'}
-            onChange={onSelect('All')}
-            disabled={isDisabled}
-          />
-        }
-        label="Everyone"
-      />
-      <FormGroup className={classes.indentedContainer}>
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={selectedDeviceType === 'All' || selectedDeviceType === 'Desktop'}
-              onChange={onSelect('Desktop')}
-              disabled={isDisabled}
-            />
-          }
-          label="Desktop"
-        />
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={selectedDeviceType === 'All' || selectedDeviceType === 'Mobile'}
-              onChange={onSelect('Mobile')}
-              disabled={isDisabled}
-            />
-          }
-          label="Mobile"
-        />
-      </FormGroup>
-    </FormGroup>
-  );
-};
+      <RadioGroup value={selectedDeviceType}>
+        <DeviceTypeRadio deviceType={'All'} isDisabled={isDisabled} onChange={onChange}/>
+        <DeviceTypeRadio deviceType={'Desktop'} isDisabled={isDisabled} onChange={onChange}/>
+        <DeviceTypeRadio deviceType={'Mobile'} isDisabled={isDisabled} onChange={onChange}/>
+      </RadioGroup>
+    </FormGroup>;
 
 export default TestEditorTargetDeviceType;
