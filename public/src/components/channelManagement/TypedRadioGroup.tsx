3d61231f9769b0e03c2ffe710d9e5418983a1efa
Tom Forbes <tom.forbes@theguardian.com>
2022-03-31 15:22:34 +0100

    shorter

--- public/src/components/channelManagement/TypedRadioGroup.tsx
+++ public/src/components/channelManagement/TypedRadioGroup.tsx
@@ -1,30 +1,6 @@
 import React from 'react';
 import { FormControlLabel, FormGroup, Radio, RadioGroup } from '@material-ui/core';
 
-interface TypedRadioProps<T extends string> {
-  value: T;
-  label: string;
-  isDisabled: boolean;
-  onChange: (updatedValue: T) => void;
-}
-function TypedRadio<T extends string>({
-  value,
-  label,
-  onChange,
-  isDisabled,
-}: TypedRadioProps<T>): React.ReactElement<TypedRadioProps<T>> {
-  return (
-    <FormControlLabel
-      value={value}
-      key={value}
-      control={<Radio />}
-      label={label}
-      disabled={isDisabled}
-      onChange={() => onChange(value)}
-    />
-  );
-}
-
 // For mapping each value in T to a user-friendly label
 type LabeledValues<T extends string> = {
   [value in T]: string;
@@ -49,12 +25,13 @@ function TypedRadioGroup<T extends string>({
       <RadioGroup value={selectedValue}>
         {(Object.entries(labels) as [T, string][]) // Cast necessary because Object.entries loses the types
           .map(([value, label]) => (
-            <TypedRadio
+            <FormControlLabel
               value={value}
+              key={value}
+              control={<Radio />}
               label={label}
-              onChange={onChange}
-              isDisabled={isDisabled}
-              key={`typed-radio-${value}`}
+              disabled={isDisabled}
+              onChange={() => onChange(value)}
             />
           ))}
       </RadioGroup>
