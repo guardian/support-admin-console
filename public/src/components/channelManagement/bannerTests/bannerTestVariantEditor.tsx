e2a80b82effac9019999ee0a548307a512241ec5
Tom Forbes <tom.forbes@theguardian.com>
2022-11-16 08:23:21 +0000

    disable

--- public/src/components/channelManagement/bannerTests/bannerTestVariantEditor.tsx
+++ public/src/components/channelManagement/bannerTests/bannerTestVariantEditor.tsx
@@ -357,7 +357,7 @@ const BannerTestVariantEditor: React.FC<BannerTestVariantEditorProps> = ({
   const classes = useStyles();
   const setValidationStatusForField = useValidation(onValidationChange);
 
-  const allowVariantTicker = true; // TODO - set based on template
+  const allowVariantTicker = false; // TODO - set based on template
 
   const onMobileContentRadioChange = (): void => {
     if (variant.mobileBannerContent === undefined) {
