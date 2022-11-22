c3b2db5782bee537adf34db0c8d90c4305c1cf74
Silvija Blaslov <silvija.blaslov@gmail.com>
2022-11-22 17:06:01 +0000

    Reading s3 data and rendering it in react

--- public/src/components/defaultPromos.tsx
+++ public/src/components/defaultPromos.tsx
@@ -1,5 +1,28 @@
 import React from 'react';
+import withS3Data, { InnerProps } from '../hocs/withS3Data';
+import {
+  fetchSupportFrontendSettings,
+  saveSupportFrontendSettings,
+  SupportFrontendSettingsType,
+} from '../utils/requests';
 
-const Hello = () => <div>Hello</div>;
+type ProductName = 'guardianWeekly' | 'paper';
 
-export default Hello;
+type DefaultPromos = {
+  [key in ProductName]: string[];
+};
+
+const DefaultPromos: React.FC<InnerProps<DefaultPromos>> = ({
+  data,
+  setData,
+  saveData,
+  saving,
+}: InnerProps<DefaultPromos>) => {
+  return <div>{JSON.stringify(data)}</div>;
+};
+
+export default withS3Data<DefaultPromos>(
+  DefaultPromos,
+  () => fetchSupportFrontendSettings(SupportFrontendSettingsType.defaultPromos),
+  data => saveSupportFrontendSettings(SupportFrontendSettingsType.defaultPromos, data),
+);
