d6e09448c0b04dc6f7fc7391a5dbbade9a9418f0
Tom Wey <tom.wey@theguardian.com>
2022-11-25 14:43:09 +0000

    Work-in-progress default promos UI
    A few things to fix still:
    
    * Add back paper promos
    * React error in browser about unmanaged components
    * Empty promo codes being sent to API (either entire input is empty or
    comma with nothing after it in input)

--- public/src/components/defaultPromos.tsx
+++ public/src/components/defaultPromos.tsx
@@ -1,4 +1,10 @@
-import React from 'react';
+import {
+  Button,
+  FormControlLabel,
+  TextField,
+  makeStyles,
+} from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
 import withS3Data, { InnerProps } from '../hocs/withS3Data';
 import {
   fetchSupportFrontendSettings,
@@ -12,13 +18,71 @@ type DefaultPromos = {
   [key in ProductName]: string[];
 };
 
+const useStyles = makeStyles(() => ({
+  container: {
+    margin: '30px',
+    maxWidth: '500px',
+    display: 'flex',
+    flexDirection: 'column',
+    '& > * + *': {
+      marginTop: '5px',
+    },
+  },
+}));
+
 const DefaultPromos: React.FC<InnerProps<DefaultPromos>> = ({
   data,
   setData,
   saveData,
   saving,
 }: InnerProps<DefaultPromos>) => {
-  return <div>{JSON.stringify(data)}</div>;
+  const [gwPromosString, setGwPromosString] = useState<string>();
+
+  useEffect(() => {
+    setGwPromosString(data.guardianWeekly.join(', '));
+  }, []);
+
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <TextField
+        value={gwPromosString}
+        name="guardianWeeklyDefaultPromos"
+        fullWidth={true}
+        onChange={e => {
+          const inputValue = e.target.value;
+          setGwPromosString(inputValue);
+
+          const parsedInputValue = inputValue.split(',').map(promo => promo.trim());
+          setData({
+            ...data,
+            guardianWeekly: parsedInputValue,
+          });
+        }}
+        type="text"
+        label="Guardian Weekly"
+      />
+      {/* <TextField
+        value={data.paper.join(', ')}
+        name="paperDefaultPromos"
+        fullWidth={true}
+        onChange={e => console.log(e.target)}
+        type="text"
+        label="Paper"
+      /> */}
+      <Button
+        onClick={saveData}
+        color="primary"
+        variant="contained"
+        size="large"
+        fullWidth={false}
+        disabled={saving}
+      >
+        Submit
+      </Button>
+    </div>
+  );
 };
 
 export default withS3Data<DefaultPromos>(
