cf67411af9a8a54a5fbef3b2e1f514cd56bf70fb
Tom Forbes <tom.forbes@theguardian.com>
2023-03-13 09:15:00 +0000

    Remove unused moment banners (#438)
    * Remove unused moment banners
    
    * more
    
    * climate
    
    * fix

--- public/src/components/channelManagement/bannerTests/bannerTestVariantEditor.tsx
+++ public/src/components/channelManagement/bannerTests/bannerTestVariantEditor.tsx
@@ -288,12 +288,7 @@ const BannerTestVariantContentEditor: React.FC<BannerTestVariantContentEditorPro
             template === BannerTemplate.GuardianWeeklyBanner ||
             template === BannerTemplate.CharityAppealBanner ||
             template === BannerTemplate.InvestigationsMomentBanner ||
-            template === BannerTemplate.ClimateCrisisMomentBanner ||
             template === BannerTemplate.GlobalNewYearBanner ||
-            template === BannerTemplate.UsEoyMomentBanner ||
-            template === BannerTemplate.UsEoyGivingTuesMomentBanner ||
-            template === BannerTemplate.AusEoyMomentBanner ||
-            template === BannerTemplate.UsEoyMomentBannerV3 ||
             template === BannerTemplate.UkraineMomentBanner) && (
             <Controller
               name="highlightedText"
@@ -365,11 +360,7 @@ const BannerTestVariantEditor: React.FC<BannerTestVariantEditorProps> = ({
   const classes = useStyles();
   const setValidationStatusForField = useValidation(onValidationChange);
 
-  const allowVariantTicker =
-    variant.template === BannerTemplate.UsEoyMomentBanner ||
-    variant.template === BannerTemplate.UsEoyGivingTuesMomentBanner ||
-    variant.template === BannerTemplate.AusEoyMomentBanner ||
-    variant.template === BannerTemplate.UsEoyMomentBannerV3;
+  const allowVariantTicker = false; // currently no banner templates support the ticker
 
   const onMobileContentRadioChange = (): void => {
     if (variant.mobileBannerContent === undefined) {
