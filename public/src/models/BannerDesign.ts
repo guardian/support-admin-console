4100f9789afef9101fa5860a01dbd923e99dac97
Tom Wey <tom.wey@theguardian.com>
2023-08-21 09:58:11 +0100

    Support for 3 different image sizes (#475)
    Adds support for multiple image URLs for different screen sizes: mobile,
    tablet/desktop and wide. From talking to Rik the convention is that 3 images
    are specified (although in the component 6 are supported, we specify 3 pairs).
    
    I've slightly re-structured the BannerDesign type to have a complex "image"
    property which contains the 3 image URLs plus the alt text.

--- public/src/models/BannerDesign.ts
+++ public/src/models/BannerDesign.ts
@@ -1,8 +1,15 @@
 import { LockStatus } from '../components/channelManagement/helpers/shared';
 
+interface BannerDesignImage {
+  mobileUrl: string;
+  tabletDesktopUrl: string;
+  wideUrl: string;
+  altText: string;
+}
+
 export interface BannerDesign {
   name: string;
-  imageUrl: string;
+  image: BannerDesignImage;
   isNew?: boolean;
   lockStatus?: LockStatus;
 }
