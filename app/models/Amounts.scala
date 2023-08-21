cab464a21894c201e09bb1849c8c2131256209f2
Tom Forbes <tom.forbes@theguardian.com>
2023-08-21 15:37:04 +0100

    Amounts targeting field

--- app/models/Amounts.scala
+++ app/models/Amounts.scala
@@ -3,10 +3,10 @@ package models
 import io.circe.generic.extras.Configuration
 import io.circe.generic.extras.auto._
 import io.circe.generic.extras.semiauto._
-import io.circe.{Decoder, Encoder}
+import io.circe.{Decoder, Encoder, Json}
 
 case class AmountValuesObject(
-    amounts: List[Int], 
+    amounts: List[Int],
     defaultAmount: Int,
     hideChooseYourAmount: Boolean,
 )
@@ -24,13 +24,33 @@ case class AmountsVariant(
   amountsCardData: AmountsCardData,
 )
 
+sealed trait AmountsTestTargeting
+case class RegionTargeting(region: Region) extends AmountsTestTargeting
+case class CountryTargeting(countryCodes: List[String]) extends AmountsTestTargeting
+
+object AmountsTestTargeting {
+  import io.circe.generic.auto._
+  import cats.syntax.functor._  // for the widen syntax
+
+  implicit val amountsTestTargetingDecoder: Decoder[AmountsTestTargeting] =
+    Decoder[RegionTargeting].widen or Decoder[CountryTargeting].widen
+
+  implicit val amountsTestTargetingEncoder: Encoder[AmountsTestTargeting] = Encoder.instance {
+    case RegionTargeting(region) => Json.fromFields(List(
+      "region" -> Json.fromString(region.toString)
+    ))
+    case CountryTargeting(countryCodes) => Json.fromFields(List(
+      "countryCodes" -> Json.fromValues(countryCodes.map(Json.fromString))
+    ))
+  }
+}
+
 case class AmountsTest(
   testName: String,
   liveTestName: Option[String],
   testLabel: Option[String],
   isLive: Boolean,
-  region: String,
-  country: List[String],
+  targeting: AmountsTestTargeting,
   order: Int,
   seed: Int,
   variants: List[AmountsVariant],
---
+++ test/models/AmountsSpec.scala
@@ -0,0 +1,23 @@
+package models
+
+import org.scalatest.EitherValues
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import io.circe.parser._
+import io.circe.syntax.EncoderOps
+import models.Region.GBPCountries
+
+class AmountsSpec extends AnyFlatSpec with Matchers with EitherValues {
+  it should "decode amounts" in {
+    decode[AmountsTestTargeting]("""{ "countryCodes": ["GB"] }""") should be(Right(CountryTargeting(List("GB"))))
+    decode[AmountsTestTargeting]("""{ "region": "GBPCountries" }""") should be(Right(RegionTargeting(GBPCountries)))
+  }
+  it should
+  "encode amounts" in {
+    val countryTargeting: AmountsTestTargeting = CountryTargeting(List("GB"))
+    countryTargeting.asJson.noSpaces should be("""{"countryCodes":["GB"]}""")
+
+    val regionTargeting: AmountsTestTargeting = RegionTargeting(GBPCountries)
+    regionTargeting.asJson.noSpaces should be("""{"region":"GBPCountries"}""")
+  }
+}
