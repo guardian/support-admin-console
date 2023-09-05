47ac469fbb226c7000fcb8b327ed9e28d46d7405
Tom Wey <tom.wey@theguardian.com>
2023-09-05 10:21:11 +0100

    Enable banner designs in CODE

--- public/src/utils/features.ts
+++ public/src/utils/features.ts
@@ -2,5 +2,5 @@ import { getStage } from './stage';
 
 export const shouldShowBannerDesignsFeature = (): boolean => {
   const stage = getStage();
-  return stage !== 'PROD' && stage !== 'CODE';
+  return stage !== 'PROD';
 };
