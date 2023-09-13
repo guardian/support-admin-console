d1e704737bb817955501e3d80458750391bc23e8
Tom Wey <tom.wey@theguardian.com>
2023-09-13 17:04:59 +0100

    Pull this code up to DynamoService
    So that it can be used from DynamoBannerDesigns.

--- app/services/DynamoChannelTests.scala
+++ app/services/DynamoChannelTests.scala
@@ -112,34 +112,6 @@ class DynamoChannelTests(stage: String, client: DynamoDbClient) extends DynamoSe
       case other => DynamoPutError(other)
     }
 
-  private def putAllTransaction(items: List[TransactWriteItem]): ZIO[ZEnv, DynamoPutError, Unit] =
-    effectBlocking {
-      val request = TransactWriteItemsRequest
-        .builder
-        .transactItems(items.asJava)
-        .build()
-
-      val result = client.transactWriteItems(request)
-      logger.info(s"TransactWriteItemsResponse: $result")
-      ()
-    }.mapError(DynamoPutError)
-
-  /**
-    * Dynamodb limits us to batches of 25 items.
-    * This function groups items into batches of 25. Each batch is sent as a transaction, and if any transaction
-    * fails then an error is returned to the client (no retries).
-    * It uses a zio stream to do this, pausing between batches to avoid any throttling and timing out after 1 minute.
-    */
-  private def putAllBatchedTransaction(items: List[TransactWriteItem]): ZIO[ZEnv, DynamoPutError, Unit] = {
-    val batches = items.grouped(BATCH_SIZE).toList
-    ZStream.fromIterable(batches)
-      .fixed(2.seconds) // wait 2 seconds between batches
-      .timeoutError(DynamoPutError(new Throwable("Timed out writing batches to dynamodb")))(1.minute)
-      .mapM(putAllTransaction)
-      .runCollect
-      .unit
-  }
-
   def getTest[T <: ChannelTest[T] : Decoder](testName: String, channel: Channel): ZIO[ZEnv, DynamoGetError, T] =
     get(testName, channel)
       .map(item => dynamoMapToJson(item).as[T])
