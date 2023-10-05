b1cc5b7c7ce8e87898fbbe0af4236908dd70ecd0
Tom Forbes <tom.forbes@theguardian.com>
2023-10-05 15:02:13 +0100

    simplify defaults

--- public/src/components/channelManagement/bannerDesigns/utils/defaults.ts
+++ public/src/components/channelManagement/bannerDesigns/utils/defaults.ts
@@ -1,7 +1,9 @@
 import { BannerDesign } from '../../../../models/bannerDesign';
 import { stringToHexColour } from '../../../../utils/bannerDesigns';
 
-export const DEFAULT_BANNER_DESIGN = {
+export const createDefaultBannerDesign = (name: string): BannerDesign => ({
+  name,
+  status: 'Draft',
   image: {
     mobileUrl:
       'https://i.guim.co.uk/img/media/35d403182e4b262d37385281b19b763ee6b32f6a/58_0_1743_1046/master/1743.png?width=930&quality=45&auto=format&s=9ecd82413fef9883c1e7a0df2bf6abb1',
@@ -13,102 +15,47 @@ export const DEFAULT_BANNER_DESIGN = {
   },
   colours: {
     basic: {
-      background: '222527',
-      bodyText: 'FFFFFF',
-      headerText: 'FFFFFF',
-      articleCountText: 'FFFFFF',
+      background: stringToHexColour('F1F8FC'),
+      bodyText: stringToHexColour('000000'),
+      headerText: stringToHexColour('000000'),
+      articleCountText: stringToHexColour('000000'),
     },
     highlightedText: {
-      text: 'FFFFFF',
-      highlight: 'FFE500',
+      text: stringToHexColour('000000'),
+      highlight: stringToHexColour('FFE500'),
     },
     primaryCta: {
       default: {
-        text: 'FFFFFF',
-        background: '0077B6',
+        text: stringToHexColour('FFFFFF'),
+        background: stringToHexColour('0077B6'),
       },
       hover: {
-        text: 'FFFFFF',
-        background: '004E7C',
+        text: stringToHexColour('FFFFFF'),
+        background: stringToHexColour('004E7C'),
       },
     },
     secondaryCta: {
       default: {
-        text: '004E7C',
-        background: 'F1F8FC',
-        border: '004E7C',
+        text: stringToHexColour('004E7C'),
+        background: stringToHexColour('F1F8FC'),
+        border: stringToHexColour('004E7C'),
       },
       hover: {
-        text: '004E7C',
-        background: 'E5E5E5',
-        border: '004E7C',
+        text: stringToHexColour('004E7C'),
+        background: stringToHexColour('E5E5E5'),
+        border: stringToHexColour('004E7C'),
       },
     },
     closeButton: {
       default: {
-        text: '052962',
-        background: 'F1F8FC',
-        border: '052962',
+        text: stringToHexColour('052962'),
+        background: stringToHexColour('F1F8FC'),
+        border: stringToHexColour('052962'),
       },
       hover: {
-        text: '052962',
-        background: 'E5E5E5',
+        text: stringToHexColour('052962'),
+        background: stringToHexColour('E5E5E5'),
       },
     },
-    guardianRoundel: 'default',
-  },
-};
-
-export const createDefaultBannerDesign = (name: string): BannerDesign => ({
-  name,
-  status: 'Draft',
-  image: DEFAULT_BANNER_DESIGN.image,
-  colours: {
-    basic: {
-      background: stringToHexColour(DEFAULT_BANNER_DESIGN.colours.basic.background),
-      bodyText: stringToHexColour(DEFAULT_BANNER_DESIGN.colours.basic.bodyText),
-      headerText: stringToHexColour(DEFAULT_BANNER_DESIGN.colours.basic.headerText),
-      articleCountText: stringToHexColour(DEFAULT_BANNER_DESIGN.colours.basic.articleCountText),
-    },
-    highlightedText: {
-      text: stringToHexColour(DEFAULT_BANNER_DESIGN.colours.highlightedText.text),
-      highlight: stringToHexColour(DEFAULT_BANNER_DESIGN.colours.highlightedText.highlight),
-    },
-    primaryCta: {
-      default: {
-        text: stringToHexColour(DEFAULT_BANNER_DESIGN.colours.primaryCta.default.text),
-        background: stringToHexColour(DEFAULT_BANNER_DESIGN.colours.primaryCta.default.background),
-      },
-      hover: {
-        text: stringToHexColour(DEFAULT_BANNER_DESIGN.colours.primaryCta.hover.text),
-        background: stringToHexColour(DEFAULT_BANNER_DESIGN.colours.primaryCta.hover.background),
-      },
-    },
-    secondaryCta: {
-      default: {
-        text: stringToHexColour(DEFAULT_BANNER_DESIGN.colours.secondaryCta.default.text),
-        background: stringToHexColour(
-          DEFAULT_BANNER_DESIGN.colours.secondaryCta.default.background,
-        ),
-        border: stringToHexColour(DEFAULT_BANNER_DESIGN.colours.secondaryCta.default.border),
-      },
-      hover: {
-        text: stringToHexColour(DEFAULT_BANNER_DESIGN.colours.secondaryCta.hover.text),
-        background: stringToHexColour(DEFAULT_BANNER_DESIGN.colours.secondaryCta.hover.background),
-        border: stringToHexColour(DEFAULT_BANNER_DESIGN.colours.secondaryCta.hover.border),
-      },
-    },
-    closeButton: {
-      default: {
-        text: stringToHexColour(DEFAULT_BANNER_DESIGN.colours.closeButton.default.text),
-        background: stringToHexColour(DEFAULT_BANNER_DESIGN.colours.closeButton.default.background),
-        border: stringToHexColour(DEFAULT_BANNER_DESIGN.colours.closeButton.default.border),
-      },
-      hover: {
-        text: stringToHexColour(DEFAULT_BANNER_DESIGN.colours.closeButton.hover.text),
-        background: stringToHexColour(DEFAULT_BANNER_DESIGN.colours.closeButton.hover.background),
-      },
-    },
-    guardianRoundel: 'default',
   },
 });
