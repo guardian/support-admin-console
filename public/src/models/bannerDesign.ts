a5b47fc484c06a866a8043f481f8b0fee7189be4
Tom Forbes <tom.forbes@theguardian.com>
2023-10-05 14:16:30 +0100

    client model

--- public/src/models/bannerDesign.ts
+++ public/src/models/bannerDesign.ts
@@ -18,10 +18,35 @@ export interface BasicColours {
   bodyText: HexColour;
 }
 
+interface CtaStateDesign {
+  text: HexColour;
+  background: HexColour;
+  border?: HexColour;
+}
+export interface CtaDesign {
+  default: CtaStateDesign;
+  hover: CtaStateDesign;
+}
+
+export type GuardianRoundel = 'default' | 'brand' | 'inverse';
+
 export type BannerDesignProps = {
   image: BannerDesignImage;
   colours: {
-    basic: BasicColours;
+    basic: {
+      background: HexColour;
+      bodyText: HexColour;
+      headerText: HexColour;
+      articleCountText: HexColour;
+    };
+    highlightedText: {
+      text: HexColour;
+      highlight: HexColour;
+    };
+    primaryCta: CtaDesign;
+    secondaryCta: CtaDesign;
+    closeButton: CtaDesign;
+    guardianRoundel?: GuardianRoundel;
   };
 };
 
