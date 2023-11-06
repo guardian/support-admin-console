0ba464736f029abe7629a452b99c702772203dd1
Rik Roots <rik.roots@gmail.com>
2023-11-06 16:58:28 +0000

    fix lint errors

--- public/src/components/amounts/AmountsTestEditor.tsx
+++ public/src/components/amounts/AmountsTestEditor.tsx
@@ -363,9 +363,9 @@ export const AmountsTestEditor: React.FC<AmountsTestEditorProps> = ({
 
         {!checkIfTestIsCountryTier() && (
           <Typography className={classes.note}>
-            Note: users arriving at the checkout page from Apple News/Google AMP article
-            CTAs will only see their region&apos;s amounts test, with options for single, 
-            monthly and annual contributions.
+            Note: users arriving at the checkout page from Apple News/Google AMP article CTAs will
+            only see their region&apos;s amounts test, with options for single, monthly and annual
+            contributions.
           </Typography>
         )}
 
