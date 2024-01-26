5b25a39c37d99415dc1a979425672b526ffe09ba
Tom Forbes <tom.forbes@theguardian.com>
2024-01-26 09:47:15 +0000

    Remove nulls when serializing to dynamodb

--- test/utils/CirceSpec.scala
+++ test/utils/CirceSpec.scala
@@ -5,7 +5,16 @@ import org.scalatest.EitherValues
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import io.circe.parser._
+import io.circe.syntax.EncoderOps
+import models.{BannerTest, BannerUI, BannerVariant}
+import models.Channel.Banner1
+import models.Status.Draft
+import models.UserCohort.AllNonSupporters
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue
+import diffson.lcs._
+import diffson.circe._
+import diffson.jsonpatch._
+import diffson.jsonpatch.lcsdiff._
 
 class CirceSpec extends AnyFlatSpec with Matchers with EitherValues {
   val rawJson =
@@ -22,11 +31,6 @@ class CirceSpec extends AnyFlatSpec with Matchers with EitherValues {
       |""".stripMargin
 
   it should "convert json to dynamo attributes, and back again" in {
-    import diffson.lcs._
-    import diffson.circe._
-    import diffson.jsonpatch._
-    import diffson.jsonpatch.lcsdiff._
-
     val initialJson: Json = parse(rawJson).toOption.get
 
     val dynamoAttributes: AttributeValue = Circe.jsonToDynamo(initialJson)
@@ -35,6 +39,66 @@ class CirceSpec extends AnyFlatSpec with Matchers with EitherValues {
 
     implicit val patience = new Patience[Json]
     val jsonDiff = diffson.diff(resultJson, initialJson)
+
+    jsonDiff should be(JsonPatch(Nil))
+  }
+
+  it should "drop nulls when serializing to dynamo attributes" in {
+    // The model contains None values, which will be serialized to JSON nulls and then dropped
+    val bannerTest = BannerTest(
+      name = "test",
+      channel = Some(Banner1),
+      status = Some(Draft),
+      lockStatus = None,
+      priority = None,
+      nickname = None,
+      userCohort = AllNonSupporters,
+      variants = List(
+        BannerVariant(
+          name = "variant1",
+          template = BannerUI.ContributionsBanner,
+          bannerContent = None,
+          mobileBannerContent = None,
+          separateArticleCount = None,
+          tickerSettings = None
+        )
+      )
+    )
+
+    val expectedJsonWithNoNulls = parse(
+      """
+        |{
+        |  "name" : "test",
+        |  "channel" : "Banner1",
+        |  "locations" : [],
+        |  "status" : "Draft",
+        |  "userCohort" : "AllNonSupporters",
+        |  "campaignName" : "NOT_IN_CAMPAIGN",
+        |  "variants" : [
+        |    {
+        |      "name" : "variant1",
+        |      "template" : "ContributionsBanner"
+        |    }
+        |  ],
+        |  "contextTargeting" : {
+        |    "tagIds" : [],
+        |    "sectionIds" : [],
+        |    "excludedTagIds" : [],
+        |    "excludedSectionIds" : []
+        |  },
+        |  "signedInStatus" : "All"
+        |}
+        |""".stripMargin
+    ).toOption.get
+
+    val jsonWithNulls = bannerTest.asJson
+
+    val dynamoAttributes: AttributeValue = Circe.jsonToDynamo(jsonWithNulls)
+
+    val resultJson: Json = Circe.dynamoToJson(dynamoAttributes)
+
+    implicit val patience = new Patience[Json]
+    val jsonDiff = diffson.diff(resultJson, expectedJsonWithNoNulls)
 
     jsonDiff should be(JsonPatch(Nil))
   }
