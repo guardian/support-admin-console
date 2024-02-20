5cd56cd93531609259f20ef3b2a25f43ac54600e
Tom Forbes <tom.forbes@theguardian.com>
2024-02-20 15:43:04 +0000

    Upgrade to react 18

--- public/src/main.tsx
+++ public/src/main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Switchboard from './components/switchboard';
@@ -245,4 +245,10 @@ const AppRouter = () => {
   );
 };
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(<AppRouter />);
+} else {
+  console.error('No root element found');
+}
