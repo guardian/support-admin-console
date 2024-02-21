742afc150dc81a7494044e0fccb8b99e1ed5e6fc
Tom Forbes <tom.forbes@theguardian.com>
2024-02-21 13:45:08 +0000

    region filter margin

--- public/src/components/channelManagement/testListSidebarFilterSelector.tsx
+++ public/src/components/channelManagement/testListSidebarFilterSelector.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { FormControl, InputLabel, Select, MenuItem, SelectChangeEvent } from '@mui/material';
 
 import { regions, regionIds, RegionsAndAll } from '../../utils/models';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles(() => ({
+  container: {
+    marginTop: '8px',
+  },
+}));
 
 interface TestListSidebarFilterSelectorProps {
   regionFilter: string;
@@ -12,8 +19,10 @@ const TestListSidebarFilterSelector: React.FC<TestListSidebarFilterSelectorProps
   regionFilter,
   handleRegionFilterChange,
 }: TestListSidebarFilterSelectorProps) => {
+  const classes = useStyles();
+
   return (
-    <FormControl fullWidth>
+    <FormControl className={classes.container} fullWidth>
       <InputLabel id="filter-region-select-label">Filter by Region</InputLabel>
       <Select
         labelId="filter-region-select-label"
