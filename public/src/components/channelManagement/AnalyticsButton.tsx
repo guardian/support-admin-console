abebf5ef85669043ebb971fa3c7f224092fd720d
Tom Forbes <tom.forbes@theguardian.com>
2024-06-19 12:24:40 +0100

    totals

--- public/src/components/channelManagement/AnalyticsButton.tsx
+++ public/src/components/channelManagement/AnalyticsButton.tsx
@@ -1,22 +1,30 @@
 import { makeStyles } from '@mui/styles';
-import {Button, Dialog, Theme, Typography} from "@mui/material";
-import React, {useEffect} from 'react';
+import {
+  Button,
+  Dialog,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  Theme,
+  Typography,
+} from '@mui/material';
+import React, { useEffect } from 'react';
 import useOpenable from '../../hooks/useOpenable';
-import { formatISO9075, subHours} from "date-fns";
-import {LineChart, CartesianGrid, Line, XAxis, YAxis, Legend} from 'recharts';
-import {Test} from "./helpers/shared";
+import { formatISO9075, subDays, subHours } from 'date-fns';
+import { LineChart, CartesianGrid, Line, XAxis, YAxis, Legend } from 'recharts';
+import { Test } from './helpers/shared';
 
 const useStyles = makeStyles(({}: Theme) => ({
   dialog: {
     padding: '10px',
   },
   heading: {
-    margin: '12px',
-    fontSize: 16,
-  },
-  variantName: {
+    margin: '6px 12px 0 12px',
     fontSize: 18,
     fontWeight: 500,
+  },
+  chartContainer: {
+    margin: '12px',
   },
 }));
 
@@ -41,7 +49,6 @@ interface ChartProps {
   variantNames: string[];
 }
 
-
 const Colours = ['red', 'blue', 'green', 'orange', 'yellow'];
 
 const Chart = ({ data, variantNames }: ChartProps) => {
@@ -63,6 +70,18 @@ const Chart = ({ data, variantNames }: ChartProps) => {
   );
 };
 
+const buildTotals = (data: GroupedVariantViews[]): { [variantName: string]: number } => {
+  const totals: { [variantName: string]: number } = {};
+  data.forEach((viewsForHour: GroupedVariantViews) => {
+    Object.entries(viewsForHour.views).forEach(([variantName, count]) => {
+      totals[variantName] = (totals[variantName] || 0) + count;
+    });
+  });
+  return totals;
+};
+
+type Range = '24Hours' | 'week';
+
 interface AnalyticsButtonProps {
   test: Test;
   channel: Channel;
@@ -75,12 +94,15 @@ export const AnalyticsButton: React.FC<AnalyticsButtonProps> = ({
   const classes = useStyles();
   const [isOpen, open, close] = useOpenable();
   const [data, setData] = React.useState<GroupedVariantViews[]>();
+  const [range, setRange] = React.useState<Range>('24Hours');
+  const [loading, setLoading] = React.useState<boolean>(false);
 
   const { name: testName, variants } = test;
 
   useEffect(() => {
+    setLoading(true);
     const now = new Date();
-    const from = subHours(now, 24);
+    const from = range === '24Hours' ? subHours(now, 24) : subDays(now, 7);
 
     fetch(
       `/frontend/analytics/${channel}/${testName}?from=${formatISO9075(from)}&to=${formatISO9075(
@@ -88,8 +110,11 @@ export const AnalyticsButton: React.FC<AnalyticsButtonProps> = ({
       )}`,
     )
       .then(resp => resp.json())
-      .then(setData);
-  }, [testName, channel]);
+      .then(data => {
+        setData(data);
+        setLoading(false);
+      });
+  }, [testName, channel, range]);
 
   return (
     <>
@@ -97,14 +122,38 @@ export const AnalyticsButton: React.FC<AnalyticsButtonProps> = ({
         Analytics
       </Button>
 
-      <Dialog open={isOpen} onClose={close} fullWidth maxWidth="xl" className={classes.dialog}>
+      <Dialog open={isOpen} onClose={close} fullWidth maxWidth="lg" className={classes.dialog}>
         <div className={classes.heading}>
-          <h3 className={classes.variantName}>{testName}</h3>
-          <h3>Impressions per hour:</h3>
+          <h3>Impressions for: {testName}</h3>
         </div>
 
-        {data && <Chart data={data} variantNames={variants.map(v => v.name)} />}
-        {!data && <Typography>Loading...</Typography>}
+        <div className={classes.chartContainer}>
+          <RadioGroup value={range} onChange={event => setRange(event.target.value as Range)}>
+            <FormControlLabel
+              value="24Hours"
+              key="24Hours"
+              control={<Radio />}
+              label="Last 24 hours"
+            />
+            <FormControlLabel value="week" key="week" control={<Radio />} label="Last week" />
+          </RadioGroup>
+          {loading && <Typography>Loading...</Typography>}
+          {data && (
+            <div>
+              <Chart data={data} variantNames={variants.map(v => v.name)} />
+              <div>
+                <h2>Total impressions:</h2>
+                {Object.entries(buildTotals(data)).map(([variantName, count]) => (
+                  <div key={variantName}>
+                    <Typography>
+                      {variantName}: {count}
+                    </Typography>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
       </Dialog>
     </>
   );
