5e53edc70f93dd98d166070b9b3e0a85d9a7a246
Tom Forbes <tom.forbes@theguardian.com>
2025-01-22 11:35:43 +0000

    Remove super mode charts, Athena

--- public/src/components/channelManagement/superMode/superModeDashboard.tsx
+++ public/src/components/channelManagement/superMode/superModeDashboard.tsx
@@ -1,36 +1,22 @@
-import React, { useState } from 'react';
-import { Typography } from '@mui/material';
+import React from 'react';
 import { makeStyles } from '@mui/styles';
-import { SuperModeRow, useSuperModeRows } from './useSuperModeRows';
-import { ArticleDataChartDialog } from './articleDataChart';
+import { useSuperModeRows } from './useSuperModeRows';
 import { SuperModeTable } from './superModeTable';
 
 const useStyles = makeStyles(() => ({
   container: {
     margin: '10px',
   },
-  info: {
-    fontSize: '14px',
-    marginBottom: '10px',
-    fontWeight: 500,
-  },
 }));
 
 export const SuperModeDashboard: React.FC = () => {
   const classes = useStyles();
-  const [selectedRow, setSelectedRow] = useState<SuperModeRow | null>(null);
 
   const rows = useSuperModeRows();
 
   return (
     <div className={classes.container}>
-      <div>
-        <Typography variant={'h3'} className={classes.info}>
-          Double click a row to see article data
-        </Typography>
-      </div>
-      <SuperModeTable rows={rows} setSelectedRow={setSelectedRow} />
-      <ArticleDataChartDialog row={selectedRow} onClose={() => setSelectedRow(null)} />
+      <SuperModeTable rows={rows} />
     </div>
   );
 };
