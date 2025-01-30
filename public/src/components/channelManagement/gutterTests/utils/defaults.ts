402549370b4ca00cf96427daf6f702c4699a8152
“LAKSHMIRPILLAI” <luxmi.r.pillai@gmail.com>
2025-01-30 10:00:27 +0000

    Lint fix

--- public/src/components/channelManagement/gutterTests/utils/defaults.ts
+++ public/src/components/channelManagement/gutterTests/utils/defaults.ts
@@ -1,6 +1,6 @@
 import { GutterTest, GutterVariant } from '../../../../models/gutter';
 import { getStage } from '../../../../utils/stage';
-import { Cta, RegionTargeting, UserCohort } from "../../helpers/shared";
+import { Cta, RegionTargeting, UserCohort } from '../../helpers/shared';
 
 export const DEFAULT_PRIMARY_CTA: Cta = {
   text: 'Support us',
