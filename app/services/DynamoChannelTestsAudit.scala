9af51d1d4f8152ec0a5f9cfc5a463a03488fb973
Tom Forbes <tom.forbes@theguardian.com>
2025-03-19 09:54:18 +0000

    Merge pull request #702 from guardian/tf-read-audits
    Tf read audits

--- app/services/DynamoChannelTestsAudit.scala
+++ app/services/DynamoChannelTestsAudit.scala
@@ -6,17 +6,19 @@ import io.circe.syntax.EncoderOps
 import io.circe.{Decoder, Encoder}
 import models.ChannelTest
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
-import software.amazon.awssdk.services.dynamodb.model._
+import software.amazon.awssdk.services.dynamodb.model.{AttributeValue, _}
 import zio.{ZEnv, ZIO}
 import models.DynamoErrors._
-import services.DynamoChannelTestsAudit.{ChannelTestAudit, getTimeToLive}
-import utils.Circe.jsonToDynamo
+import DynamoChannelTestsAudit.{ChannelTestAudit, getTimeToLive}
+import utils.Circe.{dynamoMapToJson, jsonToDynamo}
+import zio.blocking.effectBlocking
 
 import java.time.OffsetDateTime
+import scala.jdk.CollectionConverters.{ListHasAsScala, MapHasAsJava}
 
 object DynamoChannelTestsAudit {
   // The model that we write to the audit table
-  case class ChannelTestAudit[T <: ChannelTest[T] : Encoder : Decoder](
+  case class ChannelTestAudit[T : Encoder : Decoder](
     channelAndName: String,       // The partition key is the channel and test name combined
     timestamp: OffsetDateTime,    // The range key is the timestamp of the change
     ttlInSecondsSinceEpoch: Long, // Expiry time in seconds since Epoch
@@ -24,8 +26,8 @@ object DynamoChannelTestsAudit {
     item: T                       // The new state of the item being changed
   )
 
-  implicit def encoder[T <: ChannelTest[T] : Encoder : Decoder] = deriveEncoder[ChannelTestAudit[T]]
-  implicit def decoder[T <: ChannelTest[T] : Encoder : Decoder] = deriveDecoder[ChannelTestAudit[T]]
+  implicit def encoder[T : Encoder : Decoder] = deriveEncoder[ChannelTestAudit[T]]
+  implicit def decoder[T : Encoder : Decoder] = deriveDecoder[ChannelTestAudit[T]]
 
   private val RetentionPeriodInYears = 1
   def getTimeToLive(timestamp: OffsetDateTime): OffsetDateTime = timestamp.plusYears(RetentionPeriodInYears)
@@ -33,6 +35,20 @@ object DynamoChannelTestsAudit {
 
 class DynamoChannelTestsAudit(stage: String, client: DynamoDbClient) extends DynamoService(stage, client) with StrictLogging {
   protected val tableName = s"support-admin-console-channel-tests-audit-$stage"
+
+  private def getAuditsFromDynamo(channelAndName: String): ZIO[ZEnv, DynamoGetError, java.util.List[java.util.Map[String, AttributeValue]]] =
+    effectBlocking {
+      client.query(
+        QueryRequest
+          .builder
+          .tableName(tableName)
+          .keyConditionExpression("channelAndName = :channelAndName")
+          .expressionAttributeValues(Map(
+            ":channelAndName" -> AttributeValue.builder.s(channelAndName).build
+          ).asJava)
+          .build()
+      ).items
+    }.mapError(DynamoGetError)
 
   def createAudit[T <: ChannelTest[T] : Encoder : Decoder](test: T, userEmail: String): ZIO[ZEnv, DynamoError, Unit] = {
     val channelAndName = s"${test.channel.get}_${test.name}"
@@ -56,5 +72,22 @@ class DynamoChannelTestsAudit(stage: String, client: DynamoDbClient) extends Dyn
       .build()
 
     put(request)
+  }
+
+  def getAuditsForChannelTest(channel: String, name: String): ZIO[ZEnv, DynamoError, List[ChannelTestAudit[ChannelTest[_]]]] = {
+    val channelAndName = s"${channel}_$name"
+
+    getAuditsFromDynamo(channelAndName).map { results =>
+      results.asScala
+        .map(item => dynamoMapToJson(item).as[ChannelTestAudit[ChannelTest[_]]])
+        .flatMap {
+          case Right(audit) => Some(audit)
+          case Left(error) =>
+            logger.error(s"Failed to decode audit item from Dynamo: ${error.getMessage}")
+            None
+        }
+        .toList
+        .sortBy(_.timestamp)
+    }
   }
 }
