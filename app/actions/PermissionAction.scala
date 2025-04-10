89a6a420ecfd70e285d23fa2026c047b615be65b
Tom Forbes <tom.forbes@theguardian.com>
2025-04-10 10:27:10 +0100

    tests

--- app/actions/PermissionAction.scala
+++ app/actions/PermissionAction.scala
@@ -1,10 +1,11 @@
 package actions
 
+import actions.PermissionAction.checkPermission
 import com.gu.googleauth.AuthAction
 import com.gu.googleauth.AuthAction.UserIdentityRequest
 import com.typesafe.scalalogging.LazyLogging
 import play.api.mvc.{ActionBuilder, ActionFilter, AnyContent, PlayBodyParsers, Result, Results}
-import services.DynamoPermissionsCache
+import services.{DynamoPermissionsCache, UserPermissions}
 import services.UserPermissions.Permission
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -36,6 +37,35 @@ object AuthAndPermissionActions {
     new AuthAndPermissionActions(authAction, None, None)
 }
 
+object PermissionAction extends Results with LazyLogging {
+  private def logAndForbid(message: String): Result = {
+    logger.info(message)
+    Forbidden(message)
+  }
+
+  def checkPermission(
+    email: String,
+    page: String,
+    requiredPermission: Permission,
+    userPermissions: Option[List[UserPermissions.PagePermission]]
+  ): Option[Result] = {
+    userPermissions match {
+      case Some(permissions) =>
+        permissions.find(permission => permission.name == page) match {
+          case Some(userPermission) =>
+            userPermission.permission match {
+              case Permission.Write => None // user has full access
+              case Permission.Read =>
+                if (requiredPermission != Permission.Write) None
+                else Some(logAndForbid(s"Invalid permission for user ${email}, for page $page"))
+            }
+          case None => Some(logAndForbid(s"No permission found for user ${email}, for page $page"))
+        }
+      case None => Some(logAndForbid(s"No permissions found for user ${email}"))
+    }
+  }
+}
+
 class PermissionAction(
   page: String,
   requiredPermission: Permission,
@@ -44,27 +74,14 @@ class PermissionAction(
   val executionContext: ExecutionContext,
 ) extends ActionFilter[UserIdentityRequest] with Results with LazyLogging {
 
-  private def logAndForbid(message: String): Result = {
-    logger.info(message)
-    Forbidden(message)
-  }
-
   // In the filter function we return None if the user has permission to access the resource,
   // otherwise we return a 403 Forbidden to the client
   override protected def filter[A](request: UserIdentityRequest[A]): Future[Option[Result]] = Future.successful {
-    permissionsService.getPermissionsForUser(request.user.email) match {
-      case Some(permissions) =>
-        permissions.find(permission => permission.name == page) match {
-          case Some(userPermission) =>
-            userPermission.permission match {
-              case Permission.Write => None // user has full access
-              case Permission.Read =>
-                if (requiredPermission != Permission.Write) None
-                else Some(logAndForbid(s"Invalid permission for user ${request.user.email}, for page $page"))
-            }
-          case None => Some(logAndForbid(s"No permission found for user ${request.user.email}, for page $page"))
-        }
-      case None => Some(logAndForbid(s"No permissions found for user ${request.user.email}"))
-    }
+    checkPermission(
+      request.user.email,
+      page,
+      requiredPermission,
+      permissionsService.getPermissionsForUser(request.user.email)
+    )
   }
 }
