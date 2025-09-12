039028005dad326c991849761cb814030ef2e291
Tom Forbes <tom.forbes@theguardian.com>
2025-09-12 11:27:46 +0100

    Move duplicated code into DynamoService (#784)
    * Move duplicated code into DynamoService
    
    * comment
    
    * comment

--- app/services/DynamoCampaigns.scala
+++ app/services/DynamoCampaigns.scala
@@ -4,44 +4,16 @@ import com.typesafe.scalalogging.StrictLogging
 import models.{Campaign}
 import models.DynamoErrors._
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
-import software.amazon.awssdk.services.dynamodb.model.{
-  AttributeValue,
-  ConditionalCheckFailedException,
-  PutItemRequest,
-  ScanRequest
-}
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest
 import utils.Circe.{dynamoMapToJson, jsonToDynamo}
 import zio.ZIO
 import io.circe.syntax._
 
 import scala.jdk.CollectionConverters._
-import zio.ZIO.attemptBlocking
 
-class DynamoCampaigns(stage: String, client: DynamoDbClient) extends StrictLogging {
+class DynamoCampaigns(stage: String, client: DynamoDbClient) extends DynamoService(stage, client) with StrictLogging {
 
-  private val tableName = s"support-admin-console-campaigns-$stage"
-
-  private def getAll(): ZIO[Any, DynamoGetError, java.util.List[java.util.Map[String, AttributeValue]]] =
-    attemptBlocking {
-      client
-        .scan(
-          ScanRequest
-            .builder()
-            .tableName(tableName)
-            .build()
-        )
-        .items()
-    }.mapError(DynamoGetError)
-
-  private def put(putRequest: PutItemRequest): ZIO[Any, DynamoError, Unit] =
-    attemptBlocking {
-      val result = client.putItem(putRequest)
-      logger.info(s"PutItemResponse: $result")
-      ()
-    }.mapError {
-      case err: ConditionalCheckFailedException => DynamoDuplicateNameError(err)
-      case other                                => DynamoPutError(other)
-    }
+  protected val tableName = s"support-admin-console-campaigns-$stage"
 
   def getAllCampaigns(): ZIO[Any, DynamoGetError, List[Campaign]] =
     getAll().map(results =>
