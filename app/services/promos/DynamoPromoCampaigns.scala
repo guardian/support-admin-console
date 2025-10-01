13d7bcf5bd15de9ae69bdc91938cedea65b6331f
Juarez Mota <juarez.mota.mindera@guardian.co.uk>
2025-10-01 12:34:55 +0100

    feat: add get all promo campaigns

--- app/services/promos/DynamoPromoCampaigns.scala
+++ app/services/promos/DynamoPromoCampaigns.scala
@@ -2,20 +2,26 @@ package services.promo
 
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 import com.typesafe.scalalogging.StrictLogging
+import io.circe.{Decoder, Encoder}
 import models.promos.PromoCampaign
 import models.DynamoErrors.DynamoGetError
 import zio.ZIO
+import zio.ZIO.attemptBlocking
 import software.amazon.awssdk.services.dynamodb.model.QueryRequest
 import utils.Circe.dynamoMapToJson
+import io.circe.generic.auto._
+import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue
 import services.DynamoService
 import scala.jdk.CollectionConverters._
+import models.promos.PromoProduct
 
 class DynamoPromoCampaigns(stage: String, client: DynamoDbClient)
     extends DynamoService(stage, client)
     with StrictLogging {
 
   protected val tableName = s"support-admin-console-promo-campaigns-$stage"
+  private val productIndex = "product-index"
 
   private def buildQuery(campaignCode: String): QueryRequest =
     QueryRequest.builder
@@ -35,4 +41,31 @@ class DynamoPromoCampaigns(stage: String, client: DynamoDbClient)
         case Right(promoCampaign) => ZIO.succeed(promoCampaign)
         case Left(error)          => ZIO.fail(DynamoGetError(error))
       }
+
+  def getAllPromoCampaigns(promoProduct: PromoProduct): ZIO[Any, DynamoGetError, List[PromoCampaign]] =
+    attemptBlocking {
+      client
+        .query(
+          QueryRequest.builder
+            .tableName(tableName)
+            .keyConditionExpression("product = :product")
+            .indexName(productIndex)
+            .expressionAttributeValues(
+              Map(
+                ":product" -> AttributeValue.builder.s(promoProduct.toString).build
+              ).asJava
+            )
+            .build()
+        )
+        .items
+        .asScala
+        .map(item => dynamoMapToJson(item).as[PromoCampaign])
+        .flatMap {
+          case Right(promoCampaign) => Some(promoCampaign)
+          case Left(error)          =>
+            logger.error(s"Error decoding promo campaign from dynamo: $error")
+            None
+        }
+        .toList
+    }.mapError(DynamoGetError)
 }
