27348c2d5fd0b381a5c48cd9c06998acd37e4631
Anastasiia Balenko <anastasiia.balenko.mindera@guardian.co.uk>
2025-12-08 10:33:36 +0000

    removed logic for channels other than Support Landing Page

--- public/src/components/tests/variants/variantSummaryURLGeneratorButton.tsx
+++ public/src/components/tests/variants/variantSummaryURLGeneratorButton.tsx
@@ -2,23 +2,9 @@ import React from 'react';
 import { Button } from '@mui/material';
 import CopyIcon from '@mui/icons-material/CopyAll';
 import { TestPlatform, TestType } from '../../channelManagement/helpers/shared';
-import { getStage, Stage } from '../../../utils/stage';
+import { getStage } from '../../../utils/stage';
 
 export type ArticleType = 'Standard' | 'Liveblog';
-
-const ArticlePaths: Record<ArticleType, string> = {
-  Standard:
-    'world/2020/may/08/commemorating-ve-day-during-coronavirus-lockdown-somehow-the-quiet-made-it-louder',
-  Liveblog: 'world/live/2022/oct/07/nobel-peace-prize-2022-live-winners',
-};
-
-const getHostName = (stage: Stage): string => {
-  if (stage === 'PROD') {
-    return 'theguardian.com';
-  } else {
-    return 'm.code.dev-theguardian.com';
-  }
-};
 
 const getChannelName = (testType: TestType, articleType: ArticleType): string => {
   if (testType === 'EPIC' && articleType === 'Liveblog') {
@@ -39,15 +25,9 @@ const getPreviewUrl = (
   const stage = getStage();
   const channelName = getChannelName(testType, articleType);
   const queryString = `?force-${channelName}=${testName}:${variantName}`;
-
-  if (testType === 'LANDING_PAGE') {
-    // link to the support site landing page
-    return `https://support.${
-      stage !== 'PROD' ? 'code.dev-' : ''
-    }theguardian.com/contribute${queryString}`;
-  } else {
-    return `https://${getHostName(stage)}/${ArticlePaths[articleType]}${queryString}`;
-  }
+  return `https://support.${
+    stage !== 'PROD' ? 'code.dev-' : ''
+  }theguardian.com/contribute${queryString}`;
 };
 
 interface VariantSummaryURLGeneratorButtonProps {
