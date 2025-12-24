f453c84f8579545f3efe25c7ef3aff063464f702
Anastasiia Balenko <anastasiia.balenko.mindera@guardian.co.uk>
2025-12-24 09:53:33 +0000

    preview links for different promo products

--- public/src/components/promoTool/utils/previewUrl.test.ts
+++ public/src/components/promoTool/utils/previewUrl.test.ts
@@ -4,17 +4,59 @@ import { getStage } from '../../../utils/stage';
 jest.mock('../../../utils/stage');
 
 describe('getPromoPreviewUrl', () => {
-  it('should return correct URL for PROD stage', () => {
+  it('should return correct URL for PROD stage with default product', () => {
     (getStage as jest.Mock).mockReturnValue('PROD');
     expect(getPromoPreviewUrl('TEST_PROMO')).toBe(
-      'https://support.theguardian.com/p/TEST_PROMO/terms',
+      'https://support.theguardian.com/us/contribute?promoCode=TEST_PROMO',
     );
   });
 
-  it('should return correct URL for CODE stage', () => {
+  it('should return correct URL for CODE stage with default product', () => {
     (getStage as jest.Mock).mockReturnValue('CODE');
     expect(getPromoPreviewUrl('TEST_PROMO')).toBe(
-      'https://support.code.dev-theguardian.com/p/TEST_PROMO/terms',
+      'https://support.code.dev-theguardian.com/us/contribute?promoCode=TEST_PROMO',
+    );
+  });
+
+  it('should return correct URL for Weekly product in PROD', () => {
+    (getStage as jest.Mock).mockReturnValue('PROD');
+    expect(getPromoPreviewUrl('TEST_PROMO', 'Weekly')).toBe(
+      'https://support.theguardian.com/subscribe/weekly?promoCode=TEST_PROMO',
+    );
+  });
+
+  it('should return correct URL for Weekly product in CODE', () => {
+    (getStage as jest.Mock).mockReturnValue('CODE');
+    expect(getPromoPreviewUrl('TEST_PROMO', 'Weekly')).toBe(
+      'https://support.code.dev-theguardian.com/subscribe/weekly?promoCode=TEST_PROMO',
+    );
+  });
+
+  it('should return correct URL for Newspaper product in PROD', () => {
+    (getStage as jest.Mock).mockReturnValue('PROD');
+    expect(getPromoPreviewUrl('TEST_PROMO', 'Newspaper')).toBe(
+      'https://support.theguardian.com/subscribe/paper?promoCode=TEST_PROMO',
+    );
+  });
+
+  it('should return correct URL for Newspaper product in CODE', () => {
+    (getStage as jest.Mock).mockReturnValue('CODE');
+    expect(getPromoPreviewUrl('TEST_PROMO', 'Newspaper')).toBe(
+      'https://support.code.dev-theguardian.com/subscribe/paper?promoCode=TEST_PROMO',
+    );
+  });
+
+  it('should return default URL for null product', () => {
+    (getStage as jest.Mock).mockReturnValue('PROD');
+    expect(getPromoPreviewUrl('TEST_PROMO', null)).toBe(
+      'https://support.theguardian.com/us/contribute?promoCode=TEST_PROMO',
+    );
+  });
+
+  it('should return default URL for other product types', () => {
+    (getStage as jest.Mock).mockReturnValue('PROD');
+    expect(getPromoPreviewUrl('TEST_PROMO', 'SupporterPlus')).toBe(
+      'https://support.theguardian.com/us/contribute?promoCode=TEST_PROMO',
     );
   });
 });
