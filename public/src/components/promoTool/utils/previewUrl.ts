cc83c1937498a8b248bee7cb4ad73ec205ba0356
AnastasiiaBalenko <anastasiia.balenko.mindera@guardian.co.uk>
2026-01-05 09:46:28 +0000

    preview links for different promo products (#933)

--- public/src/components/promoTool/utils/previewUrl.ts
+++ public/src/components/promoTool/utils/previewUrl.ts
@@ -1,8 +1,14 @@
 import { getStage } from '../../../utils/stage';
+import { PromoProduct } from './promoModels';
 
-export const getPromoPreviewUrl = (promoCode: string): string => {
+export const getPromoPreviewUrl = (promoCode: string, product?: PromoProduct | null): string => {
   const stage = getStage();
   const domain = stage === 'PROD' ? 'support.theguardian.com' : 'support.code.dev-theguardian.com';
-
-  return `https://${domain}/p/${promoCode}/terms`;
+  if (product === 'Weekly') {
+    return `https://${domain}/subscribe/weekly?promoCode=${promoCode}`;
+  }
+  if (product === 'Newspaper') {
+    return `https://${domain}/subscribe/paper?promoCode=${promoCode}`;
+  }
+  return `https://${domain}/us/contribute?promoCode=${promoCode}`;
 };
