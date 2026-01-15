f8bc7a5ff87cf1478ca538af8f8ed74ce7026d6c
sookburt <sue.burt@guardian.co.uk>
2026-01-15 16:14:08 +0000

    Remove currently unused

--- public/src/components/channelManagement/studentLandingPage/variantEditor.tsx
+++ public/src/components/channelManagement/studentLandingPage/variantEditor.tsx
@@ -5,17 +5,12 @@ import { StudentLandingPageVariant } from '../../../models/studentLandingPage';
 
 interface StudentLandingPageVariantEditorProps {
   variant: StudentLandingPageVariant;
-  onVariantChange: (
-    update: (current: StudentLandingPageVariant) => StudentLandingPageVariant,
-  ) => void;
   editMode: boolean;
-  onDelete: () => void; // TODO: do we want this?
   onValidationChange: (isValid: boolean) => void;
 }
 
 export const VariantEditor: React.FC<StudentLandingPageVariantEditorProps> = ({
   variant,
-  onVariantChange,
   editMode,
   onValidationChange,
 }: StudentLandingPageVariantEditorProps) => {
