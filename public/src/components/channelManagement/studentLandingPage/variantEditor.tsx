a438678fea5c8941f441073e6a0ebb9466fe040b
sookburt <sue.burt@guardian.co.uk>
2026-01-27 14:46:38 +0000

    Add fields for Institution values, remove duplication for RTE constraints

--- public/src/components/channelManagement/studentLandingPage/variantEditor.tsx
+++ public/src/components/channelManagement/studentLandingPage/variantEditor.tsx
@@ -1,19 +1,33 @@
 // TODO: fix the unused variables then delete the line below.
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState } from 'react';
-import { StudentLandingPageVariant } from '../../../models/studentLandingPage';
+import { Institution, StudentLandingPageVariant } from '../../../models/studentLandingPage';
 import { makeStyles } from '@mui/styles';
 import { Theme } from '@mui/material/styles';
 import { Controller, useForm } from 'react-hook-form';
 import { RichTextEditorSingleLine } from '../richTextEditor/richTextEditor';
 import { noHtmlValidator } from '../helpers/validation';
+import { Typography } from '@mui/material';
 
 const HEADLINE_MAX_LENGTH = 65; // TODO: what should the max length of the heading be? Based on existing copy.
 const SUBHEADING_MAX_LENGTH = 210; // TODO: what should the max length of the subheading be? Based on existing with buffer.
 
+const RTEMenuConstraints = {
+  noHtml: true,
+  noBold: true,
+  noCurrencyTemplate: true,
+  noCountryNameTemplate: true,
+  noArticleCountTemplate: true,
+  noPriceTemplates: true,
+  noDateTemplate: true,
+  noDayTemplate: true,
+  noCampaignDeadlineTemplate: true,
+};
+
 interface OfferFormData {
   heading: string;
   subheading: string;
+  institution: Institution;
 }
 
 interface StudentLandingPageVariantEditorProps {
@@ -28,7 +42,7 @@ interface StudentLandingPageVariantEditorProps {
 const getUseStyles = (shouldAddPadding: boolean) => {
   const useStyles = makeStyles(({ palette, spacing }: Theme) => ({
     container: {
-      width: '100%',
+      width: '98%',
       paddingTop: shouldAddPadding ? spacing(2) : 0,
       paddingLeft: shouldAddPadding ? spacing(4) : 0,
       paddingRight: shouldAddPadding ? spacing(10) : 0,
@@ -65,6 +79,7 @@ export const VariantEditor: React.FC<StudentLandingPageVariantEditorProps> = ({
   const defaultValues: OfferFormData = {
     heading: variant.heading,
     subheading: variant.subheading,
+    institution: variant.institution,
   };
 
   const [validatedFields, setValidatedFields] = useState<OfferFormData>(defaultValues);
@@ -94,7 +109,7 @@ export const VariantEditor: React.FC<StudentLandingPageVariantEditorProps> = ({
   useEffect(() => {
     const isValid = Object.keys(errors).length === 0;
     onValidationChange(isValid);
-  }, [errors.heading]);
+  }, [errors.heading, errors.subheading, errors.institution]);
 
   const isValidField = (field: string, fieldName: string, maxLength: number) => {
     const messages = [];
@@ -128,77 +143,172 @@ export const VariantEditor: React.FC<StudentLandingPageVariantEditorProps> = ({
     return isValidField(field, 'subheading', SUBHEADING_MAX_LENGTH);
   };
 
+  const isValidAcronym = (field: string) => {
+    return isValidField(field, 'acronym', 4);
+  };
+
+  const isValidInstitutionName = (field: string) => {
+    return isValidField(field, 'name', 150);
+  };
+
+  const isValidInstitutionUrl = (field: string) => {
+    return isValidField(field, 'logoUrl', 150);
+  };
+
   return (
     <div className={classes.container}>
-      <Controller
-        name="heading"
-        control={control}
-        rules={{
-          validate: isValidHeadline,
-        }}
-        render={({ field }) => {
-          return (
-            <RichTextEditorSingleLine
-              error={errors.heading !== undefined}
-              helperText={errors.heading ? errors.heading.message || errors.heading.type : ''}
-              copyData={field.value}
-              updateCopy={(value) => {
-                field.onChange(value);
-                handleSubmit(setValidatedFields)();
-              }}
-              name="heading"
-              label="Heading"
-              disabled={!editMode}
-              rteMenuConstraints={{
-                noHtml: true,
-                noBold: true,
-                noCurrencyTemplate: true,
-                noCountryNameTemplate: true,
-                noArticleCountTemplate: true,
-                noPriceTemplates: true,
-                noDateTemplate: true,
-                noDayTemplate: true,
-                noCampaignDeadlineTemplate: true,
-              }}
-            />
-          );
-        }}
-      />
-      <Controller
-        name="subheading"
-        control={control}
-        rules={{
-          validate: isValidSubHeading,
-        }}
-        render={({ field }) => {
-          return (
-            <RichTextEditorSingleLine
-              error={errors.subheading !== undefined}
-              helperText={
-                errors.subheading ? errors.subheading.message || errors.subheading.type : ''
-              }
-              copyData={field.value}
-              updateCopy={(value) => {
-                field.onChange(value);
-                handleSubmit(setValidatedFields)();
-              }}
-              name="subheading"
-              label="Subheading"
-              disabled={!editMode}
-              rteMenuConstraints={{
-                noHtml: true,
-                noCurrencyTemplate: true,
-                noCountryNameTemplate: true,
-                noArticleCountTemplate: true,
-                noPriceTemplates: true,
-                noDateTemplate: true,
-                noDayTemplate: true,
-                noCampaignDeadlineTemplate: true,
-              }}
-            />
-          );
-        }}
-      />
+      <div className={classes.container}>
+        <Typography variant={'h4'} className={classes.sectionHeader}>
+          Institution Details
+        </Typography>
+
+        <div className={classes.container}>
+          <Controller
+            name="institution.name"
+            control={control}
+            rules={{
+              validate: isValidInstitutionName,
+            }}
+            render={({ field }) => {
+              return (
+                <RichTextEditorSingleLine
+                  error={errors.institution?.name !== undefined}
+                  helperText={
+                    errors.institution?.name
+                      ? errors.institution?.name.message || errors.institution?.name.type
+                      : ''
+                  }
+                  copyData={field.value}
+                  updateCopy={(value) => {
+                    field.onChange(value);
+                    handleSubmit(setValidatedFields)();
+                  }}
+                  name="institution.name"
+                  label="Full name of Institution"
+                  disabled={!editMode}
+                  rteMenuConstraints={RTEMenuConstraints}
+                />
+              );
+            }}
+          />
+
+          <Controller
+            name="institution.acronym"
+            control={control}
+            rules={{
+              validate: isValidAcronym,
+            }}
+            render={({ field }) => {
+              return (
+                <RichTextEditorSingleLine
+                  error={errors.institution?.acronym !== undefined}
+                  helperText={
+                    errors.institution?.acronym
+                      ? errors.institution?.acronym.message || errors.institution?.acronym.type
+                      : ''
+                  }
+                  copyData={field.value}
+                  updateCopy={(value) => {
+                    field.onChange(value);
+                    handleSubmit(setValidatedFields)();
+                  }}
+                  name="acronym"
+                  label="Acronym of Institution"
+                  disabled={!editMode}
+                  rteMenuConstraints={RTEMenuConstraints}
+                />
+              );
+            }}
+          />
+
+          <Controller
+            name="institution.logoUrl"
+            control={control}
+            rules={{
+              validate: isValidInstitutionUrl,
+            }}
+            render={({ field }) => {
+              return (
+                <RichTextEditorSingleLine
+                  error={errors.institution?.logoUrl !== undefined}
+                  helperText={
+                    errors.institution?.logoUrl
+                      ? errors.institution?.logoUrl.message || errors.institution?.logoUrl.type
+                      : ''
+                  }
+                  copyData={field.value}
+                  updateCopy={(value) => {
+                    field.onChange(value);
+                    handleSubmit(setValidatedFields)();
+                  }}
+                  name="logoUrl"
+                  label="URL for logo of Institution"
+                  disabled={!editMode}
+                  rteMenuConstraints={RTEMenuConstraints}
+                />
+              );
+            }}
+          />
+        </div>
+      </div>
+
+      <div className={classes.container}>
+        <Typography variant={'h4'} className={classes.sectionHeader}>
+          Copy
+        </Typography>
+        <div className={classes.container}>
+          <Controller
+            name="heading"
+            control={control}
+            rules={{
+              validate: isValidHeadline,
+            }}
+            render={({ field }) => {
+              return (
+                <RichTextEditorSingleLine
+                  error={errors.heading !== undefined}
+                  helperText={errors.heading ? errors.heading.message || errors.heading.type : ''}
+                  copyData={field.value}
+                  updateCopy={(value) => {
+                    field.onChange(value);
+                    handleSubmit(setValidatedFields)();
+                  }}
+                  name="heading"
+                  label="Heading"
+                  disabled={!editMode}
+                  rteMenuConstraints={RTEMenuConstraints}
+                />
+              );
+            }}
+          />
+          <Controller
+            name="subheading"
+            control={control}
+            rules={{
+              validate: isValidSubHeading,
+            }}
+            render={({ field }) => {
+              return (
+                <RichTextEditorSingleLine
+                  error={errors.subheading !== undefined}
+                  helperText={
+                    errors.subheading ? errors.subheading.message || errors.subheading.type : ''
+                  }
+                  copyData={field.value}
+                  updateCopy={(value) => {
+                    field.onChange(value);
+                    handleSubmit(setValidatedFields)();
+                  }}
+                  name="subheading"
+                  label="Subheading"
+                  disabled={!editMode}
+                  rteMenuConstraints={{ ...RTEMenuConstraints, noBold: false }}
+                />
+              );
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
