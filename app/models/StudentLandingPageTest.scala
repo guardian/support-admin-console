5a1e1653b53ac1de2ba3cd879aed918c250d7b5b
sookburt <sue.burt@guardian.co.uk>
2026-02-06 14:44:27 +0000

    Remove unnecessary RegionTargeting from model

--- app/models/StudentLandingPageTest.scala
+++ app/models/StudentLandingPageTest.scala
@@ -34,7 +34,7 @@ case class StudentLandingPageTest(
     lockStatus: Option[LockStatus],
     priority: Option[Int],
     nickname: Option[String],
-    regionTargeting: Option[RegionTargeting] = None,
+    // regionTargeting: Option[RegionTargeting] = None,
     country: String,
     variants: List[StudentLandingPageVariant],
     campaignName: Option[String] = Some("NOT_IN_CAMPAIGN"),
