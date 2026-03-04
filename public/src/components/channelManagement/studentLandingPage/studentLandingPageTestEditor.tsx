2d70b3a50def15f184edde912d2fef77e0b48809
sookburt <sue.burt@guardian.co.uk>
2026-03-04 15:20:31 +0000

    Add para to ask users to check link

--- public/src/components/channelManagement/studentLandingPage/studentLandingPageTestEditor.tsx
+++ public/src/components/channelManagement/studentLandingPage/studentLandingPageTestEditor.tsx
@@ -134,11 +134,16 @@ export const StudentLandingPageTestEditor: React.FC<
             Preview link for Promotional Materials
           </Typography>
           {!userHasTestLocked && (
-            <StudentLandingPageLinkBuilder
-              regionId={test.regionId}
-              institution={test.variants[0].institution}
-              promoCode={test.variants[0].promoCodes[0]}
-            />
+            <>
+              <StudentLandingPageLinkBuilder
+                regionId={test.regionId}
+                institution={test.variants[0].institution}
+                promoCode={test.variants[0].promoCodes[0]}
+              />
+              <p className={classes.errorText}>
+                Please check the preview link is working as expected before adding to any promotional material.
+              </p>
+            </>
           )}
           {userHasTestLocked && (
             <p className={classes.errorText}>
