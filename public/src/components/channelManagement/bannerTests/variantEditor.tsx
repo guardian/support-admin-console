f71ed8ca7145b497731ee6fc681ad9e73ebc7814
Anastasiia Balenko <anastasiia.balenko.mindera@guardian.co.uk>
2026-03-11 10:50:40 +0000

    strikeThrough to gutter and banner highlighted text

--- public/src/components/channelManagement/bannerTests/variantEditor.tsx
+++ public/src/components/channelManagement/bannerTests/variantEditor.tsx
@@ -315,7 +315,6 @@ const VariantContentEditor: React.FC<VariantContentEditorProps> = ({
                   label="Highlighted text"
                   disabled={!editMode}
                   rteMenuConstraints={{
-                    noStrikethrough: true,
                     noBold: true,
                     noCampaignDeadlineTemplate: true,
                     noProductWeeklyTemplate: true,
