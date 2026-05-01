6e3529d8112de90db9b583437a25d190cfde1ec4
sookburt <sue.burt@guardian.co.uk>
2026-05-01 12:20:20 +0100

    Adds logo dimensions to error message so they are aware up front

--- public/src/components/channelManagement/studentLandingPage/academicInstitutionDetails.tsx
+++ public/src/components/channelManagement/studentLandingPage/academicInstitutionDetails.tsx
@@ -90,6 +90,8 @@ export const AcademicInstitutionDetailEditor: React.FC<AcademicInstituteDetailEd
   const ACRONYM_MAX_LENGTH = 4;
   const INSTITUTION_MAX_LENGTH = 150;
   const LOGO_URL_MAX_LENGTH = 150;
+  const LOGO_HELPER_TEXT =
+    'Image dimensions should be 61px wide by 27px high, with a transparent background and the foreground colour needs to be white';
 
   return (
     <div className={classes.container}>
@@ -141,7 +143,7 @@ export const AcademicInstitutionDetailEditor: React.FC<AcademicInstituteDetailEd
 
         <TextField
           {...register('logoUrl', {
-            required: EMPTY_ERROR_HELPER_TEXT,
+            required: `${EMPTY_ERROR_HELPER_TEXT} - ${LOGO_HELPER_TEXT}`,
             maxLength: LOGO_URL_MAX_LENGTH,
             validate: (name) => {
               return noHtmlValidator(name);
