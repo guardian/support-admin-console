c2cf41adfaa42eb56ebcb69c7aa8db5864b176da
Anastasiia Balenko <anastasiia.balenko.mindera@guardian.co.uk>
2026-08-07 12:41:14 +0100

    controller created

---
+++ app/models/DefaultChoiceCardsSettings.scala
@@ -0,0 +1,25 @@
+package models
+
+import io.circe.{Decoder, Encoder}
+import io.circe.generic.extras.Configuration
+import io.circe.generic.extras.semiauto.{deriveConfiguredDecoder, deriveConfiguredEncoder}
+
+case class DefaultChoiceCardsSettings(
+    epic: Map[String, ChoiceCardsSettings] = Map.empty,
+    banner: Map[String, ChoiceCardsSettings] = Map.empty
+)
+
+object DefaultChoiceCardsSettings {
+  private val emptyChoiceCardsSettings = ChoiceCardsSettings(choiceCards = Nil)
+
+  val stub: DefaultChoiceCardsSettings = DefaultChoiceCardsSettings(
+    epic = Map("Default" -> emptyChoiceCardsSettings),
+    banner = Map("Default" -> emptyChoiceCardsSettings)
+  )
+
+  implicit val customConfig: Configuration = Configuration.default.withDefaults
+  implicit val decoder: Decoder[DefaultChoiceCardsSettings] =
+    deriveConfiguredDecoder[DefaultChoiceCardsSettings]
+  implicit val encoder: Encoder[DefaultChoiceCardsSettings] =
+    deriveConfiguredEncoder[DefaultChoiceCardsSettings]
+}
