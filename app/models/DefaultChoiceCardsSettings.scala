2625fefd353572c8142de6c20d879526a438d1d3
Anastasiia Balenko <anastasiia.balenko.mindera@guardian.co.uk>
2026-08-18 15:35:41 +0100

    Default choice cards: backend controller, audit metadata, and version history endpoints

---
+++ app/models/DefaultChoiceCardsSettings.scala
@@ -0,0 +1,27 @@
+package models
+
+import io.circe.{Decoder, Encoder}
+import io.circe.generic.extras.Configuration
+import io.circe.generic.extras.semiauto.{deriveConfiguredDecoder, deriveConfiguredEncoder}
+
+case class DefaultChoiceCardsSettings(
+  epic: Map[String, ChoiceCardsSettings] = Map.empty,
+  banner: Map[String, ChoiceCardsSettings] = Map.empty,
+  lastEditedBy: String = ""
+)
+
+object DefaultChoiceCardsSettings {
+  private val emptyChoiceCardsSettings = ChoiceCardsSettings(choiceCards = Nil)
+
+  val stub: DefaultChoiceCardsSettings = DefaultChoiceCardsSettings(
+    epic = Map("Default" -> emptyChoiceCardsSettings),
+    banner = Map("Default" -> emptyChoiceCardsSettings),
+    lastEditedBy = ""
+  )
+
+  implicit val customConfig: Configuration = Configuration.default.withDefaults
+  implicit val decoder: Decoder[DefaultChoiceCardsSettings] =
+    deriveConfiguredDecoder[DefaultChoiceCardsSettings]
+  implicit val encoder: Encoder[DefaultChoiceCardsSettings] =
+    deriveConfiguredEncoder[DefaultChoiceCardsSettings]
+}
