10937751face658625ec659f85ded9219c4667ba
Anastasiia Balenko <anastasiia.balenko.mindera@guardian.co.uk>
2026-08-21 14:01:26 +0100

    useVersionHistory hook refactored

--- public/src/components/defaultChoiceCards/VersionHistory.tsx
+++ public/src/components/defaultChoiceCards/VersionHistory.tsx
@@ -12,7 +12,7 @@ export const VersionHistory: React.FC = () => {
     loading,
     error,
     diffs,
-    loadingDiffs,
+    loadingDiff,
     visibleDiffVersions,
     loadVersions,
     toggleDifferences,
@@ -38,7 +38,7 @@ export const VersionHistory: React.FC = () => {
             <VersionHistoryTable
               versions={versions}
               diffs={diffs}
-              loadingDiffs={loadingDiffs}
+              loadingDiff={loadingDiff}
               visibleDiffVersions={visibleDiffVersions}
               onToggleDifferences={toggleDifferences}
             />
