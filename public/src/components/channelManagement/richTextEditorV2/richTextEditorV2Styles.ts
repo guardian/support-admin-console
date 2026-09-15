315b5c46b7985c094041fcaa31e305ae4f0457e8
Anastasiia Balenko <anastasiia.balenko.mindera@guardian.co.uk>
2026-09-15 13:55:57 +0100

    error state

--- public/src/components/channelManagement/richTextEditorV2/richTextEditorV2Styles.ts
+++ public/src/components/channelManagement/richTextEditorV2/richTextEditorV2Styles.ts
@@ -27,6 +27,16 @@ export const useRichTextEditorV2Styles = makeStyles(() => ({
     color: 'rgba(0 0 0 / 0.6)',
     margin: '0 1.5em',
   },
+  helperText: {
+    fontSize: '85%',
+    color: 'rgba(0 0 0 / 0.6)',
+    margin: '0.5em 0 0 1.5em',
+  },
+  errorText: {
+    color: 'rgba(0 0 0 / 1)',
+    backgroundColor: 'rgba(255 255 0 / 1)',
+    margin: '0.5em 0 0 1.5em',
+  },
   remirrorEditorWrapper: {
     boxSizing: 'border-box',
     minHeight: 80,
