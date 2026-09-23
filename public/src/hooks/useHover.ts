44ed1dd57ce18981c542faad68c91be67019b60b
Anastasiia Balenko <anastasiia.balenko.mindera@guardian.co.uk>
2026-09-23 16:16:10 +0100

    remove mui/styles

--- public/src/hooks/useHover.ts
+++ public/src/hooks/useHover.ts
@@ -1,6 +1,6 @@
 import { RefObject, useCallback, useEffect, useRef, useState } from 'react';
 
-const useHover = <Element extends HTMLElement>(): [RefObject<Element>, boolean] => {
+const useHover = <Element extends HTMLElement>(): [RefObject<Element | null>, boolean] => {
   const [isHovered, setIsHovered] = useState(false);
 
   const handleMouseOver = useCallback((): void => setIsHovered(true), []);
